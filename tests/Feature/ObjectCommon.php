c49210891876a89c9922513a7ad6be5c085245d4
Klaus Dimde <klaus@dimde.info>
2018-09-29 14:44:04 +0200

    Auch die Feature Testst stammen jetzt von sunhill_testcase ab

--- tests/Feature/ObjectCommon.php
+++ tests/Feature/ObjectCommon.php
@@ -7,7 +7,13 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 
-class ObjectCommon extends TestCase
+class ObjectCommon extends \Tests\sunhill_testcase
 {
-
+    protected function setUp():void {
+        parent::setUp();
+        $this->BuildTestClasses();
+        $this->clear_system_tables();
+    }
+    
+    
 }
