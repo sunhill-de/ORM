ff88faaf2cc0aa866120a5cbc6d7d3e1db61d596
Klaus Dimde <klaus@dimde.info>
2019-05-15 19:14:00 +0200

    Varchar respertiert set_maxlen

--- lib/properties/oo_property_varchar.php
+++ lib/properties/oo_property_varchar.php
@@ -7,12 +7,15 @@ class oo_property_varchar extends oo_property_field {
 	protected $type = 'varchar';
 	
 	protected $features = ['object','simple'];
+
+	protected $maxlen=255;
 	
 	public function get_maxlen() {
-	    return '255';
+	    return $this->maxlen;
 	}
 	
 	public function set_maxlen(int $value) {
+	    $this->maxlen = $value;
 	    return $this;
 	}
 }
