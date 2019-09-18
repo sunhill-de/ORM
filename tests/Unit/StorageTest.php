bce41fcc71dfeb307a89bab3a46b38a0a6f32df5
klaus dimde <klaus@dimde.info>
2019-09-18 17:03:03 +0200

    Aufbau einer Testdatenbank

--- tests/Unit/StorageTest.php
+++ tests/Unit/StorageTest.php
@@ -32,6 +32,29 @@ class StorageTest extends sunhill_testcase_db
             [7,"\\Sunhill\\Test\\ts_referenceonly",'2019-05-15 10:00:00','2019-05-15 10:00:00'],
          ]);
         $this->insert_into('dummies',['id','dummyint'],[[1,123],[2,234],[3,345],[4,456]]);
+        $this->insert_into('tags',['id','created_at','updated_at','name','options','parent_id'],
+            [
+                [1,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagA',0,0],
+                [2,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagB',0,0],
+                [3,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagC',0,2],
+                [4,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagD',0,0],
+                [5,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagE',0,0],
+                [6,'2019-05-15 10:00:00','2019-05-15 10:00:00','TagF',0,0],
+            ]);
+        $this->insert_into('tagcache',['id','name','tag_id','created_at','updated_at'],
+            [
+                [1,'TagA',1,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [2,'TagB',2,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [3,'TagC',3,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [4,'TagC.TagB',3,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [5,'TagD',4,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [6,'TagE',5,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [7,'TagF',6,'2019-05-15 10:00:00','2019-05-15 10:00:00'],
+            ]);
+        $this->insert_into('tagobjectassigns',['container_id','tag_id'],
+            [
+                [1,1]
+            ]);
     }
     
     /**
@@ -53,7 +76,8 @@ class StorageTest extends sunhill_testcase_db
     public function SimpleProvider() {
         return [
             [1,'Sunhill\\Test\\ts_dummy','dummyint',123],
-            [2,'Sunhill\\Test\\ts_dummy','dummyint',234]
+            [2,'Sunhill\\Test\\ts_dummy','dummyint',234],
+            [1,'Sunhill\\Test\\ts_dummy','tags',['TagA']]
         ];
     }
 }
