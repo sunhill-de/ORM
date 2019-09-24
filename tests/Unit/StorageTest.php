565baabd9d0e3ac0dc856ee9ed2d901b80c48d58
klaus dimde <klaus@dimde.info>
2019-09-24 11:04:06 +0200

    Einen normierten Test für das Lesen von Objekten erstellt

--- tests/Unit/StorageTest.php
+++ tests/Unit/StorageTest.php
@@ -67,10 +67,49 @@ class StorageTest extends sunhill_testcase_db
             [
                 [1,1],[1,2]
             ]);
+        $this->insert_into('objectobjectassigns',['container_id','element_id','field','index'],
+            [
+                [5,1,'parentobject',0], // parent->parentobject = dummy(1)
+                [5,2,'parentoarray',0], // parent->parentoarray[0] = dummy(2)
+                [5,3,'parentoarray',1], // parent->parentoarray[1] = dummy(3)
+                
+                [6,3,'parentobject',0],
+                [6,1,'parentoarray',0],
+                [6,2,'parentoarray',1],                
+                [6,2,'childobject',0],
+                [6,3,'childoarray',0],
+                [6,4,'childoarray',1],                    
+                [6,1,'childoarray',2],
+                
+            ]);       
+        $this->insert_into('stringobjectassigns',['container_id','element_id','field','index'],
+            [
+                [5,'ObjectString0','parentsarray',0],
+                [5,'ObjectString1','parentsarray',1],
+                [6,'Parent0','parentsarray',0],
+                [6,'Parent1','parentsarray',1],
+                [6,'Child0','childsarray',0],
+                [6,'Child1','childsarray',1],
+                [6,'Child2','childsarray',2],                
+            ]);        
         $this->insert_into('caching',['id','object_id','fieldname','value'],
             [
                 [1,5,'parentcalc','123A'],
                 [2,6,'parentcalc','234A'],
+            ]);
+        $this->insert_into('attributes',['id','name','type','allowedobjects','property'],
+            [
+                [1,'int_attribute','int',"\\Sunhill\\Test\\ts_dummy",''],
+                [2,'attribute1','int',"\\Sunhill\\Test\\ts_testparent",''],
+                [3,'attribute2','int',"\\Sunhill\\Test\\ts_testparent",''],
+                
+            ]);
+        $this->insert_into('attributevalues',['id','attribute_id','object_id','value','textvalue','created_at','updated_at'],
+            [
+                [1,1,1,111,'','2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [2,2,5,121,'','2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [3,2,6,232,'','2019-05-15 10:00:00','2019-05-15 10:00:00'],
+                [4,3,6,666,'','2019-05-15 10:00:00','2019-05-15 10:00:00']
             ]);
     }
     
@@ -90,7 +129,7 @@ class StorageTest extends sunhill_testcase_db
         $loader = new \Sunhill\Storage\storage_load($object);
         $loader->set_inheritance($object->get_inheritance(true));
         $loader->load_object($id);
-        if (preg_match('/(?P<name>\w+)\[(?P<index>\d+)\]/',$fieldname,$match)){
+        if (preg_match('/(?P<name>\w+)\[(?P<index>\w+)\]/',$fieldname,$match)){
             $name = $match['name'];
             $index = $match['index'];
             $this->assertEquals($expected,$loader->$name[$index]);
@@ -110,6 +149,8 @@ class StorageTest extends sunhill_testcase_db
             [1,'Sunhill\\Test\\ts_dummy','tags',[1,2]],                         // Werden die Tags vernünftig ausgelesen?
             [1,'Sunhill\\Test\\ts_dummy','created_at','2019-05-15 10:00:00'],   // Werden die Felder aus oo_object ausgelesen?
             [1,'Sunhill\\Test\\ts_dummy','tags[0]',1],                          // Werden Felder richtig indiziert
+            [1,'Sunhill\\Test\\ts_dummy','attributes[int_attribute]',111],
+            
             [5,'Sunhill\\Test\\ts_testparent','parentchar','ABC'],              // Werden Varcharfelder gelesen
             [5,'Sunhill\\Test\\ts_testparent','parentint',123],                 // Werden intfelder gelesen
             [5,'Sunhill\\Test\\ts_testparent','parentfloat',1.23],              // Werden Floatfelder gelesen
@@ -119,6 +160,11 @@ class StorageTest extends sunhill_testcase_db
             [5,'Sunhill\\Test\\ts_testparent','parenttime','01:11:00'],              // Werden Floatfelder gelesen
             [5,'Sunhill\\Test\\ts_testparent','parentenum','testC'],      // Werden Textfelder gelesen
             [5,'Sunhill\\Test\\ts_testparent','parentcalc','123A'],      // Werden Textfelder gelesen
+            [5,'Sunhill\\Test\\ts_testparent','parentobject',1],      // Werden Objektfelder gelesen
+            [5,'Sunhill\\Test\\ts_testparent','parentoarray',[2,3]],      // Werden Objektfelder gelesen
+            [5,'Sunhill\\Test\\ts_testparent','parentsarray',['ObjectString0','ObjectString1']],      // Werden StringArrays gelesen
+            [5,'Sunhill\\Test\\ts_testparent','attributes[attribute1]',121],      // Werden StringArrays gelesen
+            
             
             [6,'Sunhill\\Test\\ts_testchild','parentchar','DEF'],              // Werden Varcharfelder gelesen
             [6,'Sunhill\\Test\\ts_testchild','parentint',234],                 // Werden intfelder gelesen
@@ -137,6 +183,13 @@ class StorageTest extends sunhill_testcase_db
             [6,'Sunhill\\Test\\ts_testchild','childtime','18:00:00'],              // Werden Floatfelder gelesen
             [6,'Sunhill\\Test\\ts_testchild','childenum','testA'],      // Werden Textfelder gelesen
             [6,'Sunhill\\Test\\ts_testchild','parentcalc','234A'],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','parentobject',3],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','parentoarray',[1,2]],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','childobject',2],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','childoarray',[3,4,1]],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','childsarray',['Child0','Child1','Child2']],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','attributes[attribute1]',232],              // Werden Floatfelder gelesen
+            [6,'Sunhill\\Test\\ts_testchild','attributes[attribute2]',666],              // Werden Floatfelder gelesen
             
         ];
     }
