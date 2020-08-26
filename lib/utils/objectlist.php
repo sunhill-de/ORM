0b95fc52554a9e22f10474b0eacdf837d32d7468
Klaus Dimde <klaus@dimde.info>
2020-08-26 20:13:17 +0200

    Moved objectlist from sunhill_manager to sunhill

---
+++ lib/utils/objectlist.php
@@ -0,0 +1,211 @@
+<?php
+
+/**
+ @file objectlist
+ Implements the class objectlist
+ lang=en
+ */
+namespace Sunhill\Utils;
+
+use Sunhill\SunhillException;
+
+class ObjectListException extends SunhillException
+{
+}
+
+/**
+ * A class that handles list of objects and provides some additional features
+ *
+ * @author Klaus
+ *        
+ */
+class objectlist implements \countable, \ArrayAccess, \Iterator
+{
+
+    /**
+     * Stores the objects
+     */
+    private $items = [];
+
+    /**
+     * Internal pointer for foreach
+     */
+    private $pointer = 0;
+
+    /**
+     * Caches the known classes of the objects
+     */
+    private $class_cache = [];
+
+    /**
+     * Adds a new entry
+     *
+     * @param $item is
+     *            either an int or an object
+     * @param $index default
+     *            to null wich means append it to the array, otherwise replace the index
+     */
+    public function add($item, $index = null)
+    {
+        if (is_null($index)) {
+            $this->items[] = $item;
+        } else {
+            $this->items[$index] = $item;
+        }
+    }
+
+    public function empty()
+    {
+        return empty($this->items);
+    }
+
+    public function get_id(int $index)
+    {
+        if (! $this->is_valid($index)) {
+            throw new ObjectListException("Invalid index '$index'");
+        }
+        if (is_int($this->items[$index])) {
+            return $this->items[$index];
+        } else {
+            return $this->items[$index]->get_id();
+        }
+    }
+
+    /**
+     * Gets the item with the index $index.
+     * If it's not already loaded, it loads it
+     */
+    public function get(int $index)
+    {
+        if (! $this->is_valid($index)) {
+            throw new ObjectListException("Invalid index '$index'");
+        }
+        if (is_int($this->items[$index])) {
+            $this->items[$index] = \Sunhill\Objects\oo_object::load_object_of($this->items[$index]);
+        }
+        return $this->items[$index];
+    }
+
+    /**
+     * Returns the count of items in this list
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        return $this->add($value, $offset);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+
+    public function current()
+    {
+        return $this->get($this->pointer);
+    }
+
+    public function key()
+    {
+        return $this->pointer;
+    }
+
+    public function next()
+    {
+        $this->pointer ++;
+    }
+
+    public function rewind()
+    {
+        $this->pointer = 0;
+    }
+
+    public function valid(): bool
+    {
+        return $this->is_valid($this->pointer);
+    }
+
+    protected function is_valid(int $index)
+    {
+        return (($index >= 0) && ($index < count($this->items)));
+    }
+
+    public function get_class(int $index)
+    {
+        if (! isset($this->class_cache[$index])) {
+            $this->class_cache[$index] = \Sunhill\Objects\oo_object::get_class_name_of($this->get_id($index));
+        }
+        return $this->class_cache[$index];
+    }
+
+    /**
+     * Returns the distinct classes that are in this array right now
+     */
+    public function get_distinct_classes()
+    {
+        $result = [];
+        for ($i = 0; $i < count($this->items); $i ++) {
+            $class = $this->get_class($i);
+            if (! in_array($class, $result)) {
+                $result[] = $class;
+            }
+        }
+        return $result;
+    }
+
+    public function filter_class(string $class, bool $children = true)
+    {
+        $shadow_items = [];
+        $shadow_classes = [];
+        for ($i = 0; $i < count($this->items); $i ++) {
+            if ($children) {
+                if (is_a($this->get($i), $class)) {
+                    $shadow_items[] = $this->items[$i];
+                    $shadow_classes[] = $this->get_class($i);
+                }
+            } else {
+                if ($this->get_class($i) === $class) {
+                    $shadow_items[] = $this->items[$i];
+                    $shadow_classes[] = $this->class_cache[$i];
+                }
+            }
+        }
+        $this->items = $shadow_items;
+        $this->class_cache = $shadow_classes;
+    }
+
+    public function remove_class(string $class, bool $children = true)
+    {
+        $shadow_items = [];
+        $shadow_classes = [];
+        for ($i = 0; $i < count($this->items); $i ++) {
+            if ($children) {
+                if (! is_a($this->get($i), $class)) {
+                    $shadow_items[] = $this->items[$i];
+                    $shadow_classes[] = $this->get_class($i);
+                }
+            } else {
+                if ($this->get_class($i) !== $class) {
+                    $shadow_items[] = $this->items[$i];
+                    $shadow_classes[] = $this->get_class($i);
+                }
+            }
+        }
+        $this->items = $shadow_items;
+        $this->class_cache = $shadow_classes;
+    }
+}
---
+++ tests/Unit/UtilObjectlistTest.php
@@ -0,0 +1,192 @@
+<?php
+namespace Tests\Unit;
+
+/**
+ *
+ * @file UtilObjectlistTest.php
+ * lang: en
+ * dependencies: FilesystemComplexTestCase
+ */
+use Tests\DBTestCase;
+use Sunhill\Utils\objectlist;
+use Sunhill\Test\ts_dummy;
+
+class UtilObjectlistTest extends DBTestCase
+{
+
+    public function testAddObject_method_count()
+    {
+        $test = new objectlist();
+        $test->add(1);
+        $test->add(2);
+        $test->add(3);
+        $this->assertEquals(3, $test->count());
+        return $test;
+    }
+
+    /**
+     *
+     * @depends testAddObject_method_count
+     */
+    public function testAddObject_method_countdirectt($test)
+    {
+        $this->assertEquals(3, count($test));
+        return $test;
+    }
+
+    /**
+     *
+     * @depends testAddObject_method_count
+     */
+    public function testAddObject_method_get_id($test)
+    {
+        $this->assertEquals(2, $test->get_id(1));
+        return $test;
+    }
+
+    /**
+     *
+     * @depends testAddObject_method_count
+     */
+    public function testAddObject_method_get($test)
+    {
+        $this->assertEquals(234, $test->get(1)->dummyint);
+        return $test;
+    }
+
+    /**
+     *
+     * @depends testAddObject_method_count
+     */
+    public function testAddObject_method_array($test)
+    {
+        $this->assertEquals(234, $test[1]->dummyint);
+        return $test;
+    }
+
+    /**
+     *
+     * @depends testAddObject_method_count
+     */
+    public function testAddObject_method_foreach($test)
+    {
+        $result = '';
+        foreach ($test as $item) {
+            $result .= $item->dummyint;
+        }
+        $this->assertEquals('123234345', $result);
+        return $test;
+    }
+
+    public function testAddObject_array()
+    {
+        $test = new objectlist();
+        $test[] = 1;
+        $test[] = 2;
+        $test[] = 3;
+        $this->assertEquals(3, $test->count());
+        return $test;
+    }
+
+    public function testAddObject_array_mixed()
+    {
+        $test = new objectlist();
+        $dummy = \Sunhill\Objects\oo_object::load_object_of(1);
+        $test[] = $dummy;
+        $test[] = 2;
+        $test[] = 3;
+        $this->assertEquals(3, $test->count());
+        return $test;
+    }
+
+    public function testEmpty()
+    {
+        $test = new objectlist();
+        $before = $test->empty();
+        $test->add(1);
+        $this->assertEquals($test->empty(), ! $before);
+    }
+
+    protected function get_mixed_test()
+    {
+        $test = new objectlist();
+        $test[] = 1;
+        $test[] = 5;
+        $test[] = 2;
+        return $test;
+    }
+
+    public function testAddMixedClasses()
+    {
+        $test = $this->get_mixed_test();
+        $this->assertEquals(123, $test[0]->dummyint);
+        $this->assertEquals('ABC', $test[1]->parentchar);
+    }
+
+    public function testGetClass()
+    {
+        $test = $this->get_mixed_test();
+        $this->assertEquals('\Sunhill\Test\ts_dummy', $test->get_class(0));
+    }
+
+    public function testGetDistinctClasses()
+    {
+        $test = $this->get_mixed_test();
+        $this->assertEquals([
+            '\Sunhill\Test\ts_dummy',
+            '\Sunhill\Test\ts_testparent'
+        ], $test->get_distinct_classes());
+    }
+
+    protected function get_filter_test()
+    {
+        $test = new objectlist();
+        $test[] = 1;
+        $test[] = 5;
+        $test[] = 6;
+        $test[] = 2;
+        $test[] = 7;
+        return $test;
+    }
+
+    protected function get_id_list($test)
+    {
+        $result = [];
+        for ($i = 0; $i < count($test); $i ++) {
+            $result[] = $test->get_id($i);
+        }
+        return $result;
+    }
+
+    public function testFilterClass_withchildren()
+    {
+        $test = $this->get_filter_test();
+        $test->filter_class('\Sunhill\Test\ts_testparent', true);
+        $this->assertEquals([
+            5,6,7
+        ], $this->get_id_list($test));
+    }
+
+    public function testFilterClass_withoutchildren()
+    {
+        $test = $this->get_filter_test();
+        $test->filter_class('\Sunhill\Test\ts_testparent', false);
+        $this->assertEquals([
+            5,
+        ], $this->get_id_list($test));
+    }
+
+    public function testRemoveClass_withchildren()
+    {
+        $test = $this->get_filter_test();
+        $test->remove_class('\Sunhill\Test\ts_testparent', true);
+        $this->assertEquals([1,2], $this->get_id_list($test));
+    }
+
+    public function testRemoveClass_withoutchildren()
+    {
+        $test = $this->get_filter_test();
+        $test->remove_class('\Sunhill\Test\ts_testparent', false);
+        $this->assertEquals([1,6,2,7], $this->get_id_list($test));
+    }
+}
