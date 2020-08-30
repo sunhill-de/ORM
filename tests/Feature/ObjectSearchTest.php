8dcf36c10140086e2b85137ec7312a092b2222cf
Klaus Dimde <klaus@dimde.info>
2020-08-30 19:02:26 +0200

    Adjusted all results to the new result type

--- tests/Feature/ObjectSearchTest.php
+++ tests/Feature/ObjectSearchTest.php
@@ -115,7 +115,7 @@ class ObjectSearchTest extends DBTestCase
      * @group order
      */
     public function testSearchWithCombinedConditionOrder() {
-        $result = $this->simplify_result(\Tests\Feature\searchtestB::search()->where('Bint','<',603)->where('Aint','<',502)->order_by('Bchar',true)->get());
+        $result = $this->simplify_result(\Tests\Feature\searchtestB::search()->where('Bint','<',603)->where('Aint','<',502)->order_by('Bchar',false)->get());
         $this->assertEquals([11,10],$result);
     }
     
@@ -162,80 +162,80 @@ class ObjectSearchTest extends DBTestCase
      */
     public function testSimpleSearchIDs($searchclass,$field,$relation,$value,$expect) {
         $classname = "\\Tests\\Feature\\".$searchclass;
-        $result = $classname::search()->where($field,$relation,$value)->get();
+        $result = $this->simplify_result($classname::search()->where($field,$relation,$value)->get());
         $this->assertEquals($expect,$result);
     }
     
     public function SimpleProvider() {
         return [
-            ["searchtestA",'Aint','=',111,5],
-            ["searchtestA",'Aint','=',5,null],
+            ["searchtestA",'Aint','=',111,[5]],
+            ["searchtestA",'Aint','=',5,[]],
             ["searchtestA",'Aint','<',300,[5,6]],
             ["searchtestA",'Aint','>',900,[8,9]],
             ["searchtestB",'Bint','<>',602,[10,11,14,15]],
             ["searchtestA",'Aint','<',502,[5,6,7,10,11]],
-            ["searchtestC",'Bint','=',603,15],
+            ["searchtestC",'Bint','=',603,[15]],
             ["searchtestA",'Aint','in',[111,222],[5,6]],
             
-            ["searchtestA",'Achar','=','ADE',6],
+            ["searchtestA",'Achar','=','ADE',[6]],
             ["searchtestA",'Achar','=','ABC',[5,11]],
-            ["searchtestB",'Achar','=','ABC',11],
-            ["searchtestA",'Achar','=','NÜX',null],
+            ["searchtestB",'Achar','=','ABC',[11]],
+            ["searchtestA",'Achar','=','NÜX',[]],
             ["searchtestA",'Achar','<','B',[5,6,11]],
             ["searchtestA",'Achar','>','X',[8,9]],
             ["searchtestB",'Bchar','<>','CCC',[10,11,13,14,15]],
             ["searchtestA",'Achar','<','GGH',[5,6,7,10,11,15]],
-            ["searchtestC",'Achar','=','GGG',15],
+            ["searchtestC",'Achar','=','GGG',[15]],
             ["searchtestA",'Achar','in',['GGT','GGZ'],[12,13]],
             
             ["searchtestA",'Achar','begins with','A',[5,6,11]],
-            ["searchtestA",'Achar','begins with','B',7],
-            ["searchtestA",'Achar','begins with','2',null],
+            ["searchtestA",'Achar','begins with','B',[7]],
+            ["searchtestA",'Achar','begins with','2',[]],
             ["searchtestA",'Achar','ends with','Z',[8,13]],
-            ["searchtestA",'Achar','ends with','T',12],
-            ["searchtestA",'Achar','ends with','2',null],
+            ["searchtestA",'Achar','ends with','T',[12]],
+            ["searchtestA",'Achar','ends with','2',[]],
             
             ["searchtestB",'Bchar','consists','D',[13,14]],
             ["searchtestB",'Bchar','consists','C',[10,12,13]],
-            ["searchtestB",'Bchar','consists','G',15],
-            ["searchtestB",'Bchar','consists','2',null],
+            ["searchtestB",'Bchar','consists','G',[15]],
+            ["searchtestB",'Bchar','consists','2',[]],
             
-            ["searchtestA",'Acalc','=','222=ADE',6],
-            ["searchtestA",'Acalc','=','666=RRR',null],
+            ["searchtestA",'Acalc','=','222=ADE',[6]],
+            ["searchtestA",'Acalc','=','666=RRR',[]],
             ["searchtestA",'Acalc','begins with','503',[14,15]],
-            ["searchtestA",'Acalc','begins with','666',null],
-            ["searchtestA",'Acalc','begins with','222',6],
-            ["searchtestA",'Acalc','ends with','ADE',6], 
+            ["searchtestA",'Acalc','begins with','666',[]],
+            ["searchtestA",'Acalc','begins with','222',[6]],
+            ["searchtestA",'Acalc','ends with','ADE',[6]], 
              
             ["searchtestA",'tags','has','TagA',[5,6]],
-            ["searchtestA",'tags','has','TagB.TagC',6],
-            ["searchtestA",'tags','has','TagD',null],
+            ["searchtestA",'tags','has','TagB.TagC',[6]],
+            ["searchtestA",'tags','has','TagD',[]],
             ["searchtestA",'tags','has not','TagA',[7,8,9,10,11,12,13,14,15]],
             ["searchtestA",'tags','one of',['TagE','TagF'],[5,6]],
             ["searchtestA",'tags','none of',['TagE'],[6,7,8,9,10,11,12,13,14,15]],
-            ["searchtestA",'tags','all of',['TagA','TagE'],5], 
+            ["searchtestA",'tags','all of',['TagA','TagE'],[5]], 
 
             ["searchtestA",'Asarray','has','testA',[7,8]],
             ["searchtestA",'Asarray','has','testC',[8,13]],
             ["searchtestA",'Asarray','has','testC',[8,13]],
-            ["searchtestB",'Asarray','has','testC',13],
-            ["searchtestA",'Asarray','has','testD',null],
+            ["searchtestB",'Asarray','has','testC',[13]],
+            ["searchtestA",'Asarray','has','testD',[]],
             ["searchtestA",'Asarray','has not','testA',[5,6,9,10,11,12,13,14,15]],
             ["searchtestA",'Asarray','one of',['testB','testC'],[7,8,13]],
             ["searchtestA",'Asarray','none of',['testC','testA'],[5,6,9,10,11,12,14,15]],
-            ["searchtestA",'Asarray','all of',['testC','testA'],8], 
+            ["searchtestA",'Asarray','all of',['testC','testA'],[8]], 
             ["searchtestA",'Asarray','empty',null,[5,6,9,10,11,12,14,15]],
             
             ["searchtestA",'Aobject','=',1,[7,13]],
-            ["searchtestA","Aobject",'=',2,8],
-            ["searchtestB","Aobject","=",1,13],
+            ["searchtestA","Aobject",'=',2,[8]],
+            ["searchtestB","Aobject","=",1,[13]],
             ["searchtestA","Aobject","in",[1,2],[7,8,13]],
             ["searchtestA","Aobject","=",null,[5,6,9,10,11,12,14,15]],
             
-            ["searchtestA","Aoarray","has",3,9],
-            ["searchtestA","Aoarray","has",1,null],
-            ["searchtestA","Aoarray","one of",[3,1],9],
-            ["searchtestA","Aoarray","all of",[3,4],9],
+            ["searchtestA","Aoarray","has",3,[9]],
+            ["searchtestA","Aoarray","has",1,[]],
+            ["searchtestA","Aoarray","one of",[3,1],[9]],
+            ["searchtestA","Aoarray","all of",[3,4],[9]],
             ["searchtestA","Aoarray","none of",[3,4],[5,6,7,8,10,11,12,13,14,15]],
             ["searchtestB","Boarray","empty",null,[10,11,12,14,15]],
         ];
@@ -243,7 +243,7 @@ class ObjectSearchTest extends DBTestCase
     
     public function testPassObject() {
         $test = \Sunhill\Objects\oo_object::load_object_of(1);
-        $result = \Tests\Feature\searchtestA::search()->where('Aobject','=',$test)->get();
+        $result = $this->simplify_result(\Tests\Feature\searchtestA::search()->where('Aobject','=',$test)->get());
         $this->assertEquals([7,13],$result);
         
     }
@@ -317,21 +317,21 @@ class ObjectSearchTest extends DBTestCase
      */
     public function testComplexSearchIDs($searchclass,$field1,$relation1,$value1,$field2,$relation2,$value2,$expect) {
          $classname = "\\Tests\\Feature\\".$searchclass;
-        $result = $classname::search()->where($field1,$relation1,$value1)->where($field2,$relation2,$value2)->get();
+         $result = $this->simplify_result($classname::search()->where($field1,$relation1,$value1)->where($field2,$relation2,$value2)->get());
         $this->assertEquals($expect,$result);
     }
     
     public function ComplexProvider() {
         return [
-            ["searchtestA",'Aint','<',300,'Aint','<>','222',5],
-            ["searchtestA",'Aint','<',300,'Achar','=','ABC',5],
+            ["searchtestA",'Aint','<',300,'Aint','<>','222',[5]],
+            ["searchtestA",'Aint','<',300,'Achar','=','ABC',[5]],
             ["searchtestB",'Aint','>',300,'Bint','=','602',[12,13]],
-            ["searchtestA",'tags','has','TagA','Aint','<>',222,5],
-            ["searchtestA",'tags','has','TagA','tags','has','TagC',6],
-            ["searchtestA",'Acalc','<>','111=ABC','tags','has','TagA',6],
-            ["searchtestA",'Aobject','=',1,'Aint','<','502',7],
-            ["searchtestB","Boarray","empty",null,'Asarray','has','testC',null],
-            ["searchtestA",'Asarray','has','testA','Asarray','has','testC',8],
+            ["searchtestA",'tags','has','TagA','Aint','<>',222,[5]],
+            ["searchtestA",'tags','has','TagA','tags','has','TagC',[6]],
+            ["searchtestA",'Acalc','<>','111=ABC','tags','has','TagA',[6]],
+            ["searchtestA",'Aobject','=',1,'Aint','<','502',[7]],
+            ["searchtestB","Boarray","empty",null,'Asarray','has','testC',[]],
+            ["searchtestA",'Asarray','has','testA','Asarray','has','testC',[8]],
         ];
     }
     
