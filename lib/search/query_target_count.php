f508602605c4bdf069e87d207494475dde645f23
Klaus Dimde <klaus@dimde.info>
2020-08-30 18:15:06 +0200

    Added alias for the result

--- lib/search/query_target_count.php
+++ lib/search/query_target_count.php
@@ -5,6 +5,6 @@ namespace Sunhill\Search;
 class query_target_count extends query_target {
     
     public function get_query_part() {
-        return 'select count(a.id)';
+        return 'select count(a.id) as count';
     }
 }
