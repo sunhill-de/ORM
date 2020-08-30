37f0d1c1df6d20fe3b941a47aa645ad8fa49ad6d
Klaus Dimde <klaus@dimde.info>
2020-08-30 18:15:48 +0200

    Added default search order

--- lib/search/query_order.php
+++ lib/search/query_order.php
@@ -8,10 +8,17 @@ class query_order extends query_atom {
     
     protected $direction;
     
-    public function __construct(query_builder $parent_query,string $field,string $direction) {
+    public function __construct(query_builder $parent_query,string $field,$direction=true) {
         parent::__construct($parent_query);
         $this->field = $field;
-        $this->direction = $direction;
+        if (is_bool($direction)) {
+            $this->direction = $direction?'asc':'desc';
+        } else {
+            if (($direction !== 'asc') && ($direction !== 'desc')) {
+                throw new QueryException("Unknown direction '$direction'");
+            }
+            $this->direction = $direction;
+        }
     }
     
     public function get_query_part() {
