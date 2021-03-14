9e369472d8aa1f83a685156d15dbae04da744e64
Klaus Dimde <klaus@dimde.info>
2021-03-14 15:15:53 +0100

    Implemented OperatorBase with tests

--- src/Operators/OperatorBase.php
+++ src/Operators/OperatorBase.php
@@ -1,7 +1,31 @@
 <?php
 namespace Sunhill\ORM\Operators;
 
-class OperatorBase 
-{
+use \Sunhill\Basic\Utils\descriptor;
 
+abstract class OperatorBase 
+{
+    protected $commands = [];
+    
+    public function check(descriptor $descriptor) {
+        $methods = get_class_methods($this);
+        foreach ($methods as $method) {
+            if (substr($method,0,5) === 'cond_') {
+                if (!$this->$method($descriptor)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    protected function cond_command(descriptor $descriptor) {
+        return (in_array($descriptor->command,$this->commands));
+    }
+    
+    public function execute(descriptor $descriptor) {
+        return $this->do_execute($descriptor);
+    }
+    
+    abstract protected function do_execute(descriptor $descriptor);
 }
--- tests/Unit/OperatorTest.php
+++ tests/Unit/OperatorTest.php
@@ -1,10 +1,59 @@
 <?php
 namespace Sunhill\ORM\Tests\Unit;
 
-use Sunhill\ORM\Tests\TestCase;
-use Sunhill\ORM\Objects\oo_object;
+use \Sunhill\ORM\Tests\TestCase;
+use \Sunhill\ORM\Operators\OperatorBase;
+use \Sunhill\Basic\Utils\descriptor;
+
+class TestOperator extends OperatorBase {
+
+    protected $commands = ['TestA','TestB'];
+    
+    public $condition = false;
+    
+    public $flag = '';
+    
+    protected function cond_something(descriptor $descriptor) {
+        return $this->condition;
+    }
+    
+    protected function do_execute(descriptor $descriptor) {
+        $this->flag = 'executed';    
+    }
+}
 
 class OperatorTest extends TestCase
 {
+    public function testWithAction() {
+        $test = new TestOperator();
+        $test->condition = true;
+        $descriptor = new descriptor();
+        $descriptor->command = 'TestA';
+        $this->assertTrue($test->check($descriptor));
+    }
 
+    public function testWithoutAction() {
+        $test = new TestOperator();
+        $test->condition = true;
+        $descriptor = new descriptor();
+        $descriptor->command = 'TestC';
+        $this->assertFalse($test->check($descriptor));
+    }
+
+    public function testWithoutCondition() {
+        $test = new TestOperator();
+        $test->condition = false;
+        $descriptor = new descriptor();
+        $descriptor->command = 'TestA';
+        $this->assertFalse($test->check($descriptor));
+    }
+    
+    public function testExecution() {
+        $test = new TestOperator();
+        $test->condition = true;
+        $descriptor = new descriptor();
+        $descriptor->command = 'TestA';
+        $test->execute($descriptor);
+        $this->assertEquals('executed',$test->flag);
+    }
 }
