4bf85c1917b94fdb374bcad45e1c83b628e1fe55
Klaus Dimde <klaus@dimde.info>
2021-03-14 14:49:18 +0100

    Created skeletton for the operator framework

---
+++ src/Managers/operator_manager.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file operator_manager.php
+ * Provides the operator_manager class for managing the handling of operators on objects
+ * Lang en
+ * Reviewstatus: 2021-03-14
+ * Localization: unknown
+ * Documentation: unknown
+ * Tests: unknown
+ * Coverage: unknown
+ */
+namespace Sunhill\ORM\Managers;
+
+use Sunhill\ORM\ORMException;
+use Sunhill\Basic\Utils\descriptor;
+
+class operator_manager {
+ 
+}
---
+++ tests/Unit/OperatorManagerTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Sunhill\ORM\Tests\Unit;
+
+use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\Objects\oo_object;
+
+class OperatorTest extends TestCase
+{
+
+}
