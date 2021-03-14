c2e2188d862e653895e3233d1dcab99fe77c4941
Klaus Dimde <klaus@dimde.info>
2021-03-14 18:18:03 +0100

    Implemented Operators facade

--- src/Managers/operator_manager.php
+++ src/Managers/operator_manager.php
@@ -16,4 +16,29 @@ use Sunhill\Basic\Utils\descriptor;
 
 class operator_manager {
  
+    protected $operators = null;
+    
+    protected $operator_classes = [];
+    
+    /**
+     * Adds a new operator class to the manager
+     * @param string $class
+     */
+    public function add_operator(string $class) {
+        $this->operator_classes[] = $class;
+        return $this;
+    }
+    
+    /**
+     * Returns the number of registered operators
+     * @return number
+     */
+    public function get_operator_count() {
+        return count($this->operator_classes);
+    }
+    
+    public function flush() {
+        $this->operators = null;
+        $this->operator_classes = [];
+    }
 }
--- tests/Unit/OperatorManagerTest.php
+++ tests/Unit/OperatorManagerTest.php
@@ -2,9 +2,55 @@
 namespace Sunhill\ORM\Tests\Unit;
 
 use Sunhill\ORM\Tests\TestCase;
-use Sunhill\ORM\Objects\oo_object;
+use Sunhill\ORM\Managers\operator_manager;
+use Sunhill\ORM\Operators\OperatorBase;
+use Sunhill\Basic\Utils\descriptor;
+use Sunhill\ORM\Facades\Operators;
+
+class TestOperator1 extends OperatorBase {
+    
+    protected $commands = ['test1'];
+    
+    protected function do_execute(descriptor $descriptor) {
+        
+    }
+}
+
+class TestOperator2 extends OperatorBase {
+    
+    protected $commands = ['test2'];
+    
+    protected function do_execute(descriptor $descriptor) {
+        
+    }
+}
+
+class TestOperator3 extends OperatorBase {
+    
+    protected $commands = ['test1','test2'];
+    
+    protected function do_execute(descriptor $descriptor) {
+        
+    }
+}
 
 class OperatorTest extends TestCase
 {
-
+    
+    public function testOperatorManager() {
+        $manager = new operator_manager();
+        $manager
+            ->add_operator(TestOperator1::class)
+            ->add_operator(TestOperator2::class)
+            ->add_operator(TestOperator3::class);
+        $this->assertEquals(3,$manager->get_operator_count());
+    }
+    
+    public function testOperatorFacade() {
+        Operators::flush();
+        Operators::add_operator(TestOperator1::class)
+        ->add_operator(TestOperator2::class)
+        ->add_operator(TestOperator3::class);
+        $this->assertEquals(3,Operators::get_operator_count());
+    }
 }
