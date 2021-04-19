1b2aa6b10c6adb49498e11a37963d64c9f9e6d24
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-04-19 14:22:09 +0200

    Documentation update

--- src/Facades/Objects.php
+++ src/Facades/Objects.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * @file Objects.php
+ * A facade to the objects_manager
+ * @author Klaus Dimde
+ * ---------------------------------------------------------------------------------------------------------
+ * Lang en
+ * Reviewstatus: 2021-04-11
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
 namespace Sunhill\ORM\Facades;
 
 use Illuminate\Support\Facades\Facade;
