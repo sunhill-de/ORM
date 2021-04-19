29cc27c84055005c3a6601f6d88b5f83cc3bfd4a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-04-19 14:23:53 +0200

    Documentation update

--- src/Facades/Tags.php
+++ src/Facades/Tags.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * @file Tag.php
+ * A facade to the tag_manager
+ * @author Klaus Dimde
+ * ---------------------------------------------------------------------------------------------------------
+ * Lang en
+ * Reviewstatus: 2021-04-11
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
 namespace Sunhill\ORM\Facades;
 
 use Illuminate\Support\Facades\Facade;
