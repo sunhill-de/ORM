aff47569e1202576213e813ec0e256349f4dc200
Klaus Dimde <klaus@dimde.info>
2021-05-21 16:28:34 +0200

    Moved Testcases to src

--- src/Tests/DBTestCase.php
+++ src/Tests/DBTestCase.php
@@ -19,7 +19,7 @@ abstract class DBTestCase extends TestCase
             static::$db_inited = true;
             $this->do_migration();
             $this->do_seeding();
-            Classes::create_cache(dirname(__FILE__).'/Objects');
+            Classes::create_cache(dirname(__FILE__).'/../../tests/Objects');
         }
         Objects::flush_cache();
     }
