5d9c0e5392e79637575ad6ac1bc90ec1b98b3871
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-06-25 14:33:39 +0200

    Create PropertiesHavingException.php

---
+++ src/Objects/PropertiesHavingException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file ProperiesHavingException.php
+ * Defines the exception PropertiesHavingException.
+ * @author Klaus Dimde
+ * ---------------------------------------------------------------------------------------------------------
+ * Lang en
+ * Reviewstatus: 2021-06-25
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+namespace Sunhill\ORM\Objects;
+
+use Sunhill\ORM\ORMException;
+
+/**
+ * This exception should be raised for errors in propertieshaving classes
+ * @author lokal
+ */
+class PropertiesHavingException extends ORMException {}
