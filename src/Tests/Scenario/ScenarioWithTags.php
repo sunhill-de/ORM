a3003043c37021f0afb9f3833a20522f0b8cc356
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-08-27 14:17:04 +0200

    Create ScenarioWithTags.php

---
+++ src/Tests/Scenario/ScenarioWithTags.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file ScenarioWithTags.php
+ * An extension to scenarios that handle tags
+ * Lang en
+ * Reviewstatus: 2021-08-05
+ * Localization: none required
+ * Documentation: complete
+ * Tests: tests/Unit/Scenarios/ScenariosWithTagsTest.php, tests/Feature/Scenarios/ScenarioWithTagsTest.php
+ * Coverage: unknown
+ * Dependencies: Functioning tag subsystem
+ */
+
+namespace Sunhill\ORM\Tests\Scenario;
+
+use Sunhill\ORM\Facades\Classes;
+use Sunhill\Basic\SunhillException;
+
+trait ScenarioWithObjects {
+
+  /**
+   * This is called by the Test to setup set Tags
+   */
+  protected function SetUpTags() {
+    $tags = $this->GetTags();
+    foreach ($tags as $tag) {
+      $this->SetupTag($tag);
+    }
+  }
+  
+  protected function SetupTag(string $tag) {
+    
+  }
+  
+  /**
+   * This method returns an array of tags. The tags can be seperated by dots, then a tag hirarchy is build
+   * Example:
+   *  [
+   *    'TagA',
+   *    'TagB.TagC'
+   *  ]
+   */
+  abstract function GetTags();
+}
