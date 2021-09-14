1dc22a423475eb326e9e18d63c675dd97f5ea766
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-09-14 10:17:18 +0200

    Create ManagerOperatorTest.php

---
+++ tests/Unit/Managers/ManagerOperatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sunhill\ORM\Tests\Unit\Managers;
+
+use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\Managers\operator_manager;
+use Sunhill\ORM\Facades\Operators;
+
+class ManagerOperatorTest extends TestCase
+{
+         
+}
