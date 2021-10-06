4f3b0f10946be7c7d0c1553d20a43258e064aaac
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-10-06 14:25:49 +0200

    Create ObjectException.php

---
+++ src/Objects/ObjectException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file ObjectException.php
+ * Provides the base exception for objects
+ * Lang en (complete)
+ * Reviewstatus: 2021-10-06
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ * Dependencies: Objects, ORMException, base
+ * PSR-State: complete
+ */
+namespace Sunhill\ORM\Objects;
+
+use Sunhill\ORM\ORMException;
+
+/**
+ * Baseclass for errors that raise inside of oo_object
+ * @author lokal
+ */
+class ObjectException extends ORMException 
+{
+}
