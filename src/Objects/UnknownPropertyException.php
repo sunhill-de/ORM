d9bc33966d3e2a5d2d1714941c55b5aaee913ec6
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-10-06 14:28:00 +0200

    Create UnknownPropertyException.php

---
+++ src/Objects/UnknownPropertyException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file UnknownPropertyException.php
+ * Lang en (complete)
+ * Reviewstatus: 2021-10-06
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ * Dependencies: Objects, ORMException, base
+ * PSR-State: complete
+ */
+namespace Sunhill\ORM\Objects;
+
+use Sunhill\ORM\Objects\ObjectException;
+
+/**
+ * This exception indicates that an unknown property was requested
+ */
+class UnknownPropertyException extends ObjectException 
+{
+}
