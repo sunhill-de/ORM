97d0ba10842e9aa91713bdd5b5093ec513d2804f
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-10-14 13:27:11 +0200

    psr changes

---
+++ src/Properties/PropertyDate.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file PropertyDate.php
+ * Provides the property for date fields
+ * Lang en
+ * Reviewstatus: 2021-10-14
+ * Localization: complete
+ * Documentation: complete
+ * Tests: unknown
+ * Coverage: unknown
+ * PSR-State: in progress
+ */
+namespace Sunhill\ORM\Properties;
+
+/**
+ * A class for date properties
+ */
+class PropertyDate extends PropertyField {
+	
+    protected $type = 'date';
+	
+    protected $features = ['object','simple'];
+	
+    protected $validator_name = 'date_validator';	
+}
