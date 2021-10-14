d0ba8f265602f5ea7c5aa6e16b4dd0171feffae9
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-10-14 13:20:08 +0200

    psr changes

--- src/Properties/PropertyObject.php
+++ src/Properties/PropertyObject.php
@@ -1,14 +1,26 @@
 <?php
+/**
+ * @file PropertyObject.php
+ * Provides an access to a object field
+ * Lang de,en
+ * Reviewstatus: 2021-10-14
+ * Localization: none
+ * Documentation: incomplete
+ * Tests: unknown
+ * Coverage: unknown
+ * PSR-State: incomplete
+ * Dependencies: LazyIDLoading
+ */
 
 namespace Sunhill\ORM\Properties;
 
 use Illuminate\Support\Facades\DB;
 use Sunhill\ORM\Facades\Objects;
-use Sunhill\ORM\Storage\storage_base;
+use Sunhill\ORM\Storage\StorageBase;
+use Sunhill\ORM\Properties\LazyIDLoading;
 
-require_once('property_traits.php');
-
-class oo_property_object extends oo_property_field {
+class PropertyObject extends PropertyField 
+{
 	
     use LazyIDLoading;
     
@@ -20,7 +32,8 @@ class oo_property_object extends oo_property_field {
 	
 	protected $validator_name = 'object_validator';
 	
-	public function set_allowed_objects($object) {
+	public function setAllowedObjects($object) 
+	{
 	    $this->validator->set_allowed_objects($object);
 	    return $this;
 	}
@@ -30,7 +43,8 @@ class oo_property_object extends oo_property_field {
 	 * {@inheritDoc}
 	 * @see \Sunhill\ORM\Properties\Property::load()
 	 */
-	protected function do_load(storage_base $storage,$name) {
+	protected function doLoad(storage_base $storage, $name) 
+	{
         $reference = $storage->$name;
 	    if (!empty($reference)) {
 	        $this->do_set_value($reference);
@@ -43,27 +57,31 @@ class oo_property_object extends oo_property_field {
 	 * {@inheritDoc}
 	 * @see \Sunhill\ORM\Properties\Property::do_get_value()
 	 */
-	protected function &do_get_value() {
+	protected function &doGetValue() 
+	{
 	    if (is_int($this->value)) {
 	        $this->value = Objects::load($this->value);
 	    }
-        return $this->value;	    
+            return $this->value;	    
 	}
 	
-	protected function do_insert(\Sunhill\ORM\Storage\storage_base $storage,string $name) {
+	protected function doInsert(StorageBase $storage, string $name) 
+	{
 	    if (is_int($this->value)) {
-	        $storage->set_entity($name,$this->value);
+	        $storage->setEntity($name,$this->value);
 	    } else if (is_object($this->value)){
-	        $storage->set_entity($name,$this->value->get_id());
+	        $storage->setEntity($name,$this->value->getID());
 	    }
 	}
 	
-	public function inserting(\Sunhill\ORM\Storage\storage_base $storage) {
-	    $this->commit_child_if_loaded($this->value);
+	public function inserting(StorageBase $storage) 
+	{
+	    $this->commitChildIfLoaded($this->value);
 	}
 
-	public function inserted(\Sunhill\ORM\Storage\storage_base $storage) {
-	    $this->commit_child_if_loaded($this->value);	    
+	public function inserted(StorageBase $storage) 
+	{
+	    $this->commitChildIfLoaded($this->value);	    
 	}
 	
 	/**
@@ -74,27 +92,31 @@ class oo_property_object extends oo_property_field {
 	 * @param int $type Soll bei Objekten nur die ID oder das gesamte Objekt zurückgegeben werden
 	 * @return void[]|\Sunhill\ORM\Properties\Property[]
 	 */
-	public function get_diff_array(int $type=PD_VALUE) {
-	    $diff = parent::get_diff_array($type);
+	public function getDiffArray(int $type = PD_VALUE) 
+	{
+	    $diff = parent::getDiffArray($type);
 	    if ($type == PD_ID) {
 	        return [
-	            'FROM'=>$this->get_local_id($this->shadow),
-	            'TO'=>$this->get_local_id($this->value)
+	            'FROM'=>$this->getLocalID($this->shadow),
+	            'TO'=>$this->getLocalID($this->value)
 	        ];
 	    } else {
 	        return $diff;
 	    }
 	}
 	
-	public function updating(\Sunhill\ORM\Storage\storage_base $storage) {
+	public function updating(StorageBase $storage) 
+	{
         $this->inserting($storage);
 	}
 	
-	public function updated(\Sunhill\ORM\Storage\storage_base $storage) {
+	public function updated(StorageBase $storage) 
+	{
 	    $this->updating($storage);
 	}
 	
-	protected function value_changed($from,$to) {
+	protected function valueChanged($from, $to) 
+	{
 	    foreach ($this->hooks as $hook) {
 	        $to->add_hook($hook['action'],$hook['hook'],$hook['subaction'],$hook['target']);
 	    }
