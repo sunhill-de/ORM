87e8d0c348dfaac83e03898479691f92596c98ab
klaus dimde <klaus@dimde.info>
2022-02-24 14:24:49 +0100

    seed respect the uuid field

--- database/seeds/SearchObjectsTableSeeder.php
+++ database/seeds/SearchObjectsTableSeeder.php
@@ -9,21 +9,21 @@ class SearchObjectsTableSeeder extends Seeder {
 	public function run() {
 	    DB::table('objects')->truncate();
 	    DB::table('objects')->insert([
-	        ['id'=>1,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>2,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>3,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>4,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],      
-	        ['id'=>5,'classname'=>"searchtestA",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>6,'classname'=>"searchtestA",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>7,'classname'=>"searchtestA",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>8,'classname'=>"searchtestA",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>9,'classname'=>"searchtestA",'created_at'=>'2019-05-15 10:00:00'],	        
-	        ['id'=>10,'classname'=>"searchtestB",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>11,'classname'=>"searchtestB",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>12,'classname'=>"searchtestB",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>13,'classname'=>"searchtestB",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>14,'classname'=>"searchtestB",'created_at'=>'2019-05-15 10:00:00'],	        
-	        ['id'=>15,'classname'=>"searchtestC",'created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>1,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>2,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>3,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>4,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],      
+	        ['id'=>5,'classname'=>"searchtestA",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>6,'classname'=>"searchtestA",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>7,'classname'=>"searchtestA",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>8,'classname'=>"searchtestA",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>9,'classname'=>"searchtestA",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],	        
+	        ['id'=>10,'classname'=>"searchtestB",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>11,'classname'=>"searchtestB",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>12,'classname'=>"searchtestB",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>13,'classname'=>"searchtestB",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>14,'classname'=>"searchtestB",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],	        
+	        ['id'=>15,'classname'=>"searchtestC",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
 	    ]);
 	}
 }
--- database/seeds/SimpleObjectsTableSeeder.php
+++ database/seeds/SimpleObjectsTableSeeder.php
@@ -9,13 +9,13 @@ class SimpleObjectsTableSeeder extends Seeder {
 	public function run() {
 	    DB::table('objects')->truncate();
 	    DB::table('objects')->insert([
-	        ['id'=>1,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>2,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>3,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>4,'classname'=>"dummy",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>5,'classname'=>"testparent",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>6,'classname'=>"testchild",'created_at'=>'2019-05-15 10:00:00'],
-	        ['id'=>7,'classname'=>"passthru",'created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>1,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>2,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>3,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>4,'classname'=>"dummy",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>5,'classname'=>"testparent",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>6,'classname'=>"testchild",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
+	        ['id'=>7,'classname'=>"passthru",'uuid'=>'123','created_at'=>'2019-05-15 10:00:00'],
 		]);
 	}
 }
