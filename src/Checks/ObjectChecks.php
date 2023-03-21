3215c3804929dc01686e4c623842b7ae178a4a7f
Klaus Dimde <klaus@dimde.info>
2023-03-21 15:17:50 +0100

    Implemented missing tests

--- src/Checks/ObjectChecks.php
+++ src/Checks/ObjectChecks.php
@@ -121,16 +121,27 @@ class ObjectChecks extends ChecksBase
     }
     
     /**
-     * Checks if all container objects in the stringobjectassigns table exists
+     * Checks if all container objects in the objectobjectassigns table exists
      * @return unknown
      */
-    public function check_StringObjectAssignsContainerExist(bool $repair) 
+    public function check_StringObjectAssignsContainerExist(bool $repair)
     {
         if ($entries = $this->checkForDanglingPointers('stringobjectassigns','container_id','objects','id',true)) {
-            $this->fail(__("Objects ':entries' dont exist.",array('entries'=>$entries)));
+            if ($repair) {
+                $entries = $this->repairDanglingPointers('stringobjectassigns','container_id','objects','id');
+                $this->repair(__(':entries element objects are missing in the stringobjectassigns table',['entries'=>$entries]));
+            } else {
+                $this->fail(__(':entries element objects are missing in the stringobjectassigns table',['entries'=>$entries]));
+            }
         } else {
             $this->pass();
         }
+    }
+    
+    protected function getDistinctClasses()
+    {
+        $tables = DB::table('objects')->distinct('classname')->get();
+        return $tables;
     }
     
     /**
@@ -139,87 +150,23 @@ class ObjectChecks extends ChecksBase
      */
     public function check_ObjectExistance(bool $repair) 
     {
-        $tables = DB::table('objects')->distinct('classname')->get();
-        $bad_classes = '';
-        foreach ($tables as $table) {
-            if (!Classes::searchClass($table->classname)) {
-                $bad_classes .= (empty($bad_classes)?'':', ').$table->classname;
+        $classes = $this->getDistinctClasses();        
+        $bad_classes = [];
+        foreach ($classes as $class) {
+            if (!Classes::searchClass($class->classname)) {
+                $bad_classes[] = $class->classname;
             }
         }
         if (empty($bad_classes)) {
             $this->pass();
         } else {
-            $this->fail(__("Classes ':bad_classes' dont exist.",['bad_classes'=>$bad_classes]));            
-        }
-    }
-    
-    public function check_ClassTableGaps(bool $repair) 
-    {
-        $table_tree = $this->getTableTree();
-        $return = '';
-        
-        foreach ($table_tree as $master=>$table) {
-            if ($result = $this->testTable($master,$table)) {
-                $return .= (empty($return)?'':',').$result;        
+            if ($repair) {
+               DB::table('objects')->whereIn('classname', $bad_classes)->delete();                 
+               $this->repair(__(":bad_classes non existing objects removed.",['bad_classes'=>count($bad_classes)]));
+            } else {
+                $this->fail(__(":bad_classes in objects don't exist.",['bad_classes'=>count($bad_classes)]));                
             }
         }
-        
-        if (empty($return)) {
-            $this->pass();
-        } else {
-            $this->fail(__("Objects ':return' have gaps.",['return'=>$return]));
-        }
     }
     
-    private function tableExists(string $table): bool 
-    {
-        $query = Schema::getAllTables();
-        foreach ($query as $found_table) {
-            if ($table == $found_table->name) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private function testTable(string $master,array $tables) 
-    {
-        if (!$this->tableExists($master)) {
-            return;
-        }
-        $query = DB::table($master);
-        foreach ($tables as $table) {
-            $query = $query->leftJoin($table,$table.'.id','=',$master.'.id');
-            $query = $query->orWhere("$table.id",null);
-        }
-        $result = $query->get();
-        if ($result->count() > 0) {
-            return $master;
-        }
-    }
-    
-    private function getSubtables(string $class): array 
-    {        
-        $parent_classes = Classes::getInheritanceOfClass($class);
-        $result = [];
-        
-        foreach ($parent_classes as $parent) {
-            $result[] = Classes::getTableOfClass($parent);
-        }
-        
-        return $result;
-    }
-    
-    private function getTableTree() 
-    {
-        $classes = Classes::getAllClasses();
-        $tables = [];
-        foreach ($classes as $class) {
-            if ($class['name'] !== 'object') {
-                $tables[Classes::getTableOfClass($class['name'])] = $this->getSubtables($class['name']);
-            }
-        }
-        
-        return $tables;
-    }
 }
