d575effdd8d3dde0bfa827af85abcf06b6ee087e
Klaus Dimde <klaus@dimde.info>
2023-05-03 21:47:46 +0200

    Implemented drop()

---
+++ src/Storage/Mysql/MysqlDrop.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Sunhill\ORM\Storage\Mysql;
+
+use Illuminate\Support\Facades\DB;
+use Sunhill\ORM\Facades\Classes;
+use Illuminate\Support\Facades\Schema;
+
+
+class MysqlDrop
+{
+    
+    use ClassTables;
+    
+    public function __construct(public $storage) {}
+
+    protected $id = 0;
+    
+    public function doDrop()
+    {
+        $this->additional_tables = $this->collectAdditionalTables();
+        $this->deleteParentDatasets();
+        $this->deleteTags();
+        $this->deleteAttributes();
+        
+        $this->dropChildren();
+    }
+
+    protected function dropChildren()
+    {
+        $calling_class = $this->storage->getCaller()::getInfo('name');
+        $children = Classes::getChildrenOfClass($calling_class);
+        $children[$calling_class] = [];
+        foreach ($children as $child => $info) {
+            $this->dropClass($child);
+        }        
+    }
+    
+    protected function dropClass(string $child)
+    {
+        $this->deleteHelpingTables($child);
+        $this->deleteCoreTable($child);
+        $this->deleteObjects($child);
+    }
+
+    protected function deleteParentDatasets()
+    {
+        $hirarchy = $this->storage->getInheritance();
+        array_pop($hirarchy);   // remove object
+        array_shift($hirarchy); // remove self
+        $classname = $this->storage->getCaller()::getInfo('name');
+        
+        foreach ($hirarchy as $parent) {
+            $table = Classes::getTableOfClass($parent);
+            DB::table($table)->whereIn('id',function ($query) use ($classname) {
+               $query->select('id')->from('objects')->where('classname',$classname); 
+            })->delete();
+        }
+    }
+    
+    protected function deleteTags()
+    {
+        $classname = $this->storage->getCaller()::getInfo('name');
+        
+        DB::table('tagobjectassigns')->whereIn('container_id',function($query) use ($classname) {
+            $query->select('id')->from('objects')->where('classname',$classname);            
+        })->delete();    
+    }
+    
+    protected function deleteAttributes()
+    {
+        $classname = $this->storage->getCaller()::getInfo('name');
+        
+        DB::table('attributevalues')->whereIn('object_id',function($query) use ($classname) {
+            $query->select('id')->from('objects')->where('classname',$classname);
+        })->delete();
+        
+    }
+    
+    protected function deleteObjects($child)
+    {
+        DB::table('objects')->where('classname',$child)->delete();
+    }
+    
+    protected function deleteHelpingTables($child)
+    {
+        $class_tables = $this->collectClassTables($child);
+        foreach ($class_tables['array'] as $table) {
+            Schema::drop($table);
+        }
+        foreach ($class_tables['calc'] as $table) {
+            Schema::drop($table);
+        }
+    }
+    
+    protected function deleteCoreTable($child)
+    {
+        Schema::drop(Classes::getTableOfClass($child));
+    }
+    
+}
---
+++ tests/Unit/Storage/MysqlStorageDropTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use Sunhill\ORM\Tests\DatabaseTestCase;
+use Sunhill\ORM\Tests\Testobjects\Dummy;
+use Sunhill\ORM\Tests\Testobjects\DummyChild;
+use Sunhill\ORM\Tests\Testobjects\TestParent;
+use Sunhill\ORM\Tests\Testobjects\TestChild;
+use Sunhill\ORM\Storage\Mysql\MysqlStorage;
+
+class MysqlStorageDropTest extends DatabaseTestCase
+{
+    
+    public function testDummyChildDelete()
+    {
+        $object = new DummyChild();
+        $test = new MysqlStorage($object);
+        
+        $test->drop();
+        $this->assertDatabaseHasNotTable('dummychildren');
+        $this->assertDatabaseHasTable('dummies');
+        $this->assertDatabaseMissing('objects', ['class'=>'dummychild']);
+    }
+    
+    public function testDummy()
+    {
+        $object = new Dummy();
+        $test = new MysqlStorage($object);
+        
+        $test->drop();
+        
+        $this->assertDatabaseMissing('tagobjectassigns', ['container_id'=>1]);
+        $this->assertDatabaseMissing('attributevalues', ['object_id'=>1]);
+        $this->assertDatabaseMissing('objects', ['classname'=>'dummy']);
+        $this->assertDatabaseMissing('objects', ['classname'=>'dummychild']);
+        $this->assertDatabaseHasNotTable('dummies');
+    }
+
+    public function testTestChild()
+    {
+        $object = new TestChild();
+        $test = new MysqlStorage($object);
+        
+        $test->drop();
+
+        $this->assertDatabaseHasNotTable('testchildren');        
+        $this->assertDatabaseMissing('testparents',['id'=>18]);
+        $this->assertDatabaseHasNotTable('testchildren_array_childsarray');
+        $this->assertDatabaseHasNotTable('testchildren_array_childoarray');
+        $this->assertDatabaseHasNotTable('testchildren_calc_childcalc');        
+    }
+
+    public function testTestParent()
+    {
+        $object = new TestParent();
+        $test = new MysqlStorage($object);
+        
+        $test->drop();
+        
+        $this->assertDatabaseHasNotTable('testchildren');
+        $this->assertDatabaseHasNotTable('testparents');
+
+        $this->assertDatabaseHasNotTable('testchildren_array_childsarray');
+        $this->assertDatabaseHasNotTable('testchildren_array_childoarray');
+        $this->assertDatabaseHasNotTable('testchildren_calc_childcalc');
+        
+        $this->assertDatabaseHasNotTable('testparents_array_parentsarray');
+        $this->assertDatabaseHasNotTable('testparents_array_parentoarray');
+        $this->assertDatabaseHasNotTable('testparents_calc_parentcalc');
+        
+    }
+    
+}
