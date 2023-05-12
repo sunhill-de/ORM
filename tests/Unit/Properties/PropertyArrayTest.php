bba2540bc9ecee5be8564de7cf8311efd65be23c
Klaus Dimde <klaus@dimde.info>
2023-05-12 10:06:01 +0200

    New tests for arrays and validation

---
+++ tests/Unit/Properties/PropertyArrayTest.php
@@ -0,0 +1,99 @@
+<?php
+
+use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\Properties\PropertyArray;
+use Sunhill\ORM\Properties\PropertyException;
+use Sunhill\ORM\Properties\PropertyInteger;
+
+class PropertyArrayTest extends TestCase
+{
+ 
+    public function testArrayCount()
+    {
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $test->getValue()[] = 1;
+        $test->getValue()[] = 2;
+        $test->getValue()[] = 3;
+        
+        $this->assertEquals(3,count($test->getValue()));
+        $this->assertEquals(2,$test->getValue()[1]);
+    }
+    
+    public function testWrongIndex()
+    {
+        $this->expectException(PropertyException::class);
+
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $test->getValue()[] = 1;
+        $test->getValue()[] = 2;
+        $test->getValue()[] = 3;
+        
+        $a = $test->getValue()[4];
+    }
+    
+    public function testEmpty()
+    {
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $this->assertTrue($test->empty());
+
+        $test->getValue()[] = 1;
+        $test->getValue()[] = 2;
+        $test->getValue()[] = 3;
+        
+        $this->assertFalse($test->empty());
+        
+        $test->clear();
+        
+        $this->assertTrue($test->empty());
+    }
+    
+    public function testIsElementIn()
+    {
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $this->assertTrue($test->empty());
+        
+        $test->getValue()[] = 1;
+        $test->getValue()[] = 2;
+        $test->getValue()[] = 3;
+        
+        $this->assertTrue($test->isElementIn(2));
+        $this->assertFalse($test->isElementIn(5));
+    }
+    
+    public function testValidator()
+    {
+        $this->expectException(PropertyException::class);
+        
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $test->getValue()[] = 'ABC';        
+    }
+    
+    public function testForeach()
+    {
+        $test = new PropertyArray();
+        $test->setElementType(PropertyInteger::class);
+        
+        $test->getValue()[] = 1;
+        $test->getValue()[] = 2;
+        $test->getValue()[] = 3;
+
+        $key_result = '';
+        $element_result = '';
+        foreach ($test->getValue() as $key => $element) {
+            $key_result .= $key;
+            $element_result .= $element;
+        }
+        $this->assertEquals("123",$element_result);
+        $this->assertEquals("012",$key_result);
+    }
+}
---
+++ tests/Unit/Properties/PropertyValidateTest.php
@@ -0,0 +1,182 @@
+<?php
+
+use Sunhill\ORM\Semantic\Name;
+use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\Properties\Property;
+use Sunhill\ORM\Properties\PropertyBoolean;
+use Sunhill\ORM\Properties\PropertyDate;
+use Sunhill\ORM\Properties\PropertyException;
+use Sunhill\ORM\Properties\PropertyFloat;
+use Sunhill\ORM\Properties\PropertyInteger;
+use Sunhill\ORM\Properties\PropertyObject;
+use Sunhill\ORM\Units\None;
+use Sunhill\ORM\Facades\Classes;
+use Sunhill\ORM\Objects\ORMObject;
+use Sunhill\ORM\Tests\Testobjects\Dummy;
+use Sunhill\ORM\Tests\Testobjects\DummyChild;
+use Sunhill\ORM\Properties\PropertyVarchar;
+use Sunhill\ORM\Tests\Testobjects\TestParent;
+
+class PropertyValidateTest extends TestCase
+{
+ 
+    /**
+     * @dataProvider ValidateProvider
+     * 
+     * @param unknown $property_class
+     * @param unknown $postfix
+     * @param unknown $value
+     * @param unknown $expect
+     */
+    public function testValidate($property_class, $postfix, $value, $expect)
+    {
+        Classes::registerClass(Dummy::class);
+        Classes::registerClass(DummyChild::class);
+        Classes::registerClass(TestParent::class);
+        
+        $property = new $property_class();
+        if (is_callable($postfix)) {
+            $postfix($property);
+        }
+        if (is_callable($value)) {
+            $this->assertEquals($expect, $property->isValid($value()));
+        } else {
+            $this->assertEquals($expect, $property->isValid($value));            
+        }
+    }
+    
+    public function ValidateProvider()
+    {
+        return [
+           // ========================== Integer ===============================
+            [PropertyInteger::class, null, 1, true],
+            [PropertyInteger::class, null, 1.1, false],
+            [PropertyInteger::class, null, 'A', false],
+            [PropertyInteger::class, null, '1', true],
+
+            // ========================== Float ===============================
+            [PropertyFloat::class, null, 1, true],
+            [PropertyFloat::class, null, 1.1, true],
+            [PropertyFloat::class, null, "1", true],
+            [PropertyFloat::class, null, "1.1", true],
+            [PropertyFloat::class, null, "A", false],
+            [PropertyFloat::class, null, "1.1.1", false],
+
+            // ========================== Varchar ===============================
+            [PropertyVarchar::class, null, 1, true],
+            [PropertyVarchar::class, null, 1.1, true],
+            [PropertyVarchar::class, null, "1", true],
+            
+            // ========================== Boolean ===============================
+            [PropertyBoolean::class, null, 'Y', true],
+            [PropertyBoolean::class, null, 'N', true],
+            [PropertyBoolean::class, null, '+', true],
+            [PropertyBoolean::class, null, '-', true],
+            [PropertyBoolean::class, null, 'true', true],
+            [PropertyBoolean::class, null, 'false', true],
+            [PropertyBoolean::class, null, true, true],
+            [PropertyBoolean::class, null, false, true],
+            [PropertyBoolean::class, null, 'ABC', false],
+            [PropertyBoolean::class, null, 1, true],
+            [PropertyBoolean::class, null, 0, true],
+            [PropertyBoolean::class, null, 10, true],
+            
+            // =========================== Date ================================
+            [PropertyDate::class, null, '1.2.2018', true],
+            [PropertyDate::class, null, '2018-2-1', true],
+            [PropertyDate::class, null, 23.3, true],
+            [PropertyDate::class, null, '2018-2', true],
+            [PropertyDate::class, null, '2.3.', true],
+            [PropertyDate::class, null, '2018', true],
+            [PropertyDate::class, null, false, false],            
+            [PropertyDate::class, null, 'ABC', false],
+            [PropertyDate::class, null, '', false],
+            
+            // ========================== Object ===============================
+            [
+                PropertyObject::class, 
+                function(&$property) { 
+                    $property->setAllowedObjects('dummy'); 
+                }, 
+                function() { 
+                    return new Dummy(); 
+                }, true
+            ],
+            [
+                PropertyObject::class,
+                function(&$property) {
+                    $property->setAllowedObjects('dummy');
+                },
+                function() {
+                    return new TestParent();
+                }, false
+            ],
+            [
+                PropertyObject::class,
+                function(&$property) {
+                    $property->setAllowedObjects('dummy');
+                },
+                function() {
+                    return new DummyChild();
+                }, true
+            ],
+            [
+                PropertyObject::class,
+                function(&$property) {
+                    $property->setAllowedObjects('dummychild');
+                },
+                function() {
+                    return new Dummy();
+                }, false
+            ],
+            [
+                PropertyObject::class,
+                function(&$property) {
+                    $property->setAllowedObjects('dummy','testparent');
+                },
+                function() {
+                    return new Dummy();
+                }, true
+            ],
+            [
+                PropertyObject::class,
+                function(&$property) {
+                    $property->setAllowedObjects('dummychild','testparent');
+                },
+                function() {
+                    return new Dummy();
+                }, false
+            ],
+          ];
+    }
+    
+    /**
+     * @dataProvider ConvertValueProvider
+     */
+    public function testConvertValue($property_class, $postfix, $value, $expect)
+    {
+        Classes::registerClass(Dummy::class);
+        Classes::registerClass(DummyChild::class);
+        Classes::registerClass(TestParent::class);
+        
+        $property = new $property_class();
+        if (is_callable($postfix)) {
+            $postfix($property);
+        }
+        if (is_callable($value)) {
+            $this->assertEquals($expect, $property->convertValue($value()));
+        } else {
+            $this->assertEquals($expect, $property->convertValue($value));
+        }        
+    }
+    
+    public function ConvertValueProvider()
+    {
+        return [
+            [PropertyInteger::class, null, 1, 1],            
+            [PropertyFloat::class, null, 1.1, 1.1],
+            [PropertyVarchar::class, null, "A", "A"],
+            [PropertyVarchar::class, function($property) { $property->setMaxLen(2); }, "ABCD", "AB"],
+        ];
+    }
+}
