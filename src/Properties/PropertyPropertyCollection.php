34211655c3755ec79539be75b4dda62c0bb527f0
Klaus Dimde <klaus@dimde.info>
2023-05-13 20:21:18 +0200

    Implemented Property for Collection

---
+++ src/Properties/PropertyPropertyCollection.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @file PropertyObject.php
+ * Provides an access to a object field
+ * Lang de,en
+ * Reviewstatus: 2021-10-14
+ * Localization: none
+ * Documentation: incomplete
+ * Tests: unknown
+ * Coverage: unknown
+ * PSR-State: incomplete
+ * Dependencies: LazyIDLoading
+ */
+
+namespace Sunhill\ORM\Properties;
+
+use Illuminate\Support\Facades\DB;
+use Sunhill\ORM\Facades\Objects;
+use Sunhill\ORM\Storage\StorageBase;
+use Sunhill\ORM\Properties\LazyIDLoading;
+
+class PropertyPropertyCollection extends AtomarProperty
+{
+	
+    use ClassCheck;
+    
+	protected static $type = 'integer';
+		
+	protected $initialized = true;
+	
+	public function isValid($input): bool
+	{
+	    return $this->isAllowedObject($input);
+	}
+	
+	public function convertValue($input)
+	{
+	    if (is_numeric($input)) {
+	        return Objects::load($input);	        
+	    }
+	    return $input;
+	}
+	
+	public function loadFromStorage(StorageBase $storage) 
+	{
+        $name = $this->getName();
+	    $object_id = $storage->$name;	    
+	    if (!empty($object_id)) {
+	        $this->doSetValue($this->convertValue($object_id));
+	    }
+	}
+	
+	public function insertIntoStorage(StorageBase $storage) 
+	{
+        $storage->setEntity($this->getName(), $this->value->getID());
+	}
+
+	public function updateToStorage(StorageBase $storage)
+	{
+	    if ($this->isDirty()) {
+	    }
+	}
+}
