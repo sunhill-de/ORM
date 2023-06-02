285604ea731562f86d7a17b220aff95362e48b38
Klaus Dimde <klaus@dimde.info>
2023-06-02 13:37:29 +0200

    Move TagManager tests to feature and cut down the TagManager to the necessary parts

--- src/Managers/TagManager.php
+++ src/Managers/TagManager.php
@@ -30,378 +30,70 @@ define('TagNamespace','Sunhill\ORM\Objects\Tag');
 class TagManager 
 {
  
-    /**
-     * Takes the result of a mysql query and forms it into a result Descriptor
-     * @param $result stdobject The result of the mysql query
-     * @return \Manager\Utils\Descriptor
-     */
-   private function getQueryDescriptor($result): Descriptor 
-   {
-        $part = new Descriptor();
-        $part->set_id($result->id)->set_name($result->name)->set_parent_id($result->parent_id)
-                 ->set_parent_name($result->parent_name)->set_fullpath(static::getTagFullpath($result->id));
-        return $part;
-    }
-
-    private function prepareQuery() {
-         return DB::table('tags as a')->select(['a.id as id','a.name as name','a.parent_id as parent_id','b.name as parent_name'])
-                ->leftJoin('tags as b','b.id','=','a.parent_id');
-    }
-
-// ===================== Handling of all tags ==============================     
      /**
-      * Return the tag with ID $id 
-      * @param int $id
-      * @return Descriptor
+      * Searches for all tags that fit to given $input string as name.
+      * If you search for "TagA" it will find "TagA" and "TagB.TagA" (but not ("TagA.TagB")
+      *  
+      * @param unknown $input
+      * @return \Illuminate\Support\Collection
       */
-     public function getTag(int $id) 
+     public function searchTag(string $input)
      {
-         $query = static::prepareQuery()->where('a.id',$id)->first();
-         if (empty($query)) {
-            return null;            
-         }
-         return $this->getQueryDescriptor($query);
+        return $this->query()->where('any_path',$input)->get();    
      }
      
      /**
-      * Returns the fullpath of a tag (that is all parenttags in a row seperated with a dot)
-      * @param int $id
-      * @return string
+      * Searches for the Tag that fits best to the given input.
+      * If input is a int it assumes this as the id of the tag (and loads it)
+      * If input is already a Tag there is nothing to do
+      * If input is a string then it searches for the Tag that fits to this string (@see searchTag())
+      * 
+      * @param unknown $input
+      * @throws TagException When the search fails or is not unique 
+      * @return NULL|\Sunhill\ORM\Objects\Tag|unknown
       */
-     public function getTagFullpath(int $id): string 
+     public function getTag($input): Tag
      {
-         $result = '';
-         while ($id) {
-             $query = DB::table('tags')->where('id',$id)->first();
-             $id = $query->parent_id;
-             if (empty($result)) {                 
-                 $result = $query->name;
-             } else {
-                 $result = $query->name.'.'.$result;
+         if (is_int($input)) {
+             $tag = $this->loadTag($input);
+             if (empty($tag)) {
+                 throw new TagException("Tag with id '$input' was not found.");                 
              }
-         }
-         return $result;
-     }
-     
-     /**
-      * Changes the tag with the id $id
-      * @param int $id
-      * @param array $change
-      */
-     public function changeTag(int $id, array $change) 
-     {
-         $dbchange = $this->getDBChange($change);
-         $this->updateDatabase($id,$dbchange);
-         $this->updateTagcache($id,$dbchange);
-         $this->updateDependencies($id,$change);
-     }
-
-     protected function updateDependencies(int $id, array $change) 
-     {
-         
-     }
-     
-     /**
-      * Decides what kind of information is passed with param $tag and returns the corresponding 
-      * tag Descriptor array
-      * @param unknown $tag
-      * @return Descriptor
-      */
-     public function findTag($tag) 
-     {
-         if (is_a($tag,TagNamespace)) {
-             return $this->getTag($tag->id);
-         } else if (is_int($tag)) {
-             // It should be the ID of the Tag
-             return $this->getTag($tag);
-         } else if (is_string($tag)) {             // It should be the Name of the Tag             
-             return $this->searchTag($tag);
-         }
-     }
-     
-     private function getDBChange(array $change) 
-     {
-         $result = [];
-         foreach ($change as $key=>$value) {
-             if ($key == 'parent') {
-                 $search = static::searchTag($value);
-                 $result['parent_id'] = $search->id;
-             } else {
-                 $result[$key] = $value;
+             return $this->loadTag($input);
+         } else if (is_a($input,Tag::class)) {
+             return $input;
+         } else if (is_string($input)) {
+             $result = $this->searchTag($input);
+             if (count($result) == 0) {
+                 throw new TagException("Tag '$input' not found.");
+             } else if (count($result) > 1) {
+                 throw new TagException("Tag '$input' not unique.");                 
              }
+             return $this->loadTag($result[0]->id);
          }
-         return $result;
-     }
-     
-     private function updateDatabase(int $id, array $change) 
-     {
-         DB::table('tags')->where('id',$id)->update($change);
-     }
-     
-     private function updateTagcache(int $id, array $change) 
-     {
-         static::deleteCache($id);
-         static::addCacheEntry($id);
-     }
-     
-     private function addCacheEntry(int $id) 
-     {
-         $fullpath = static::getTagFullpath($id);
-         $parts = explode('.',$fullpath);
-         $tag_name = '';
-         do {
-             $rest = array_pop($parts);
-             if (!empty($tag_name)) {
-                 $tag_name = $rest.'.'.$tag_name;
-             } else {
-                 $tag_name = $rest;
-             }
-             db::table('tagcache')->insert(['tag_id'=>$id,'path_name'=>$tag_name,'is_fullpath'=>empty($parts)]);
-         } while (!empty($parts));         
-     }
-     
-    public function clearTags() 
-    {
-         $this->clearDependencies();
-         $this->clearCache();
-         $this->clearDB();         
      }
      
      /**
-      * Clears the tag-object-association
-      */
-     protected function clearDependencies() 
-     {
-        DB::table('tagobjectassigns')->delete();
-     }
-    
-     /**
-      * Clears the tag cache
-      */
-     protected function clearCache() 
-     {
-        DB::table('tagcache')->delete();
-     }
-    
-     /**
-      * Clears the tags table
-      */
-     protected function clearDB() 
-     {
-        DB::table('tags')->delete();
-     }
-    
-     /**
-      * Deletes the tag with the id $id
+      * Loads the tag with the given id when it exists.
+      * 
       * @param int $id
+      * @return \Sunhill\ORM\Objects\Tag|NULL return null when this tag doesnt exist.
       */
-     public function deleteTag(int $id) 
+     public function loadTag(int $id)
      {
-         $this->deleteDependencies($id);
-         static::deleteCache($id);
-         static::deleteDB($id);
-     }
-     
-     private function deleteDependentTags(int $id)
-     {
-         $result = DB::table('tags')->where('parent_id',$id)->get();
-         foreach ($result as $tag) {
-             $this->deleteTag($tag->id);
-         }         
-     }
-     
-     private function deleteObjectAssigns(int $id)
-     {
-        DB::table('tagobjectassigns')->where('tag_id')->delete();
-     }
-     
-     private function deleteDependencies(int $id) 
-     {
-        $this->deleteDependentTags($id);
-        $this->deleteObjectAssigns($id);
-     }
-     
-     private function deleteCache(int $id) 
-     {
-         DB::table('tagcache')->where('tag_id',$id)->delete();         
-     }
-          
-     private function deleteDB(int $id) 
-     {
-         DB::table('tags')->where('id',$id)->delete();
-         DB::table('tagobjectassigns')->where('tag_id',$id)->delete();
-     }
-     
-     /**
-      * Adds a tag with the given values
-      * @param array $values
-      */
-     public function addTag($taginfo) 
-     {
-        if (is_array($taginfo)) {
-            $this->addTagByArray($taginfo);
-        } else if (is_a($taginfo,Descriptor::class)) {
-            $this->addTagByDescriptor($taginfo);
-        } else if (is_string($taginfo)) {
-            $this->addTagByString($taginfo);
-        } else if (is_a($taginfo,Tag::class)) {
-            $this->addTagByObject($taginfo);
-        } else {
-            throw new TagException(__("Unkown data passed to 'add_tag'."));
-        }
-     }
-    
-    /**
-     * Returns the id of the given tag or 0 if null is passed
-     */
-    protected function getTagID($parent) 
-    {
-        if (is_null($parent) || empty($parent)) {
-            return 0;
-        } else {
-            return $this->loadTag($parent)->getID();
-        }
-    }
-    
-    /**
-     * Adds a tag with the given information to the tags table and add the necessary entries in the tagcache
-     * @param $name string The name of the tag
-     * @param $parent null|string|Tag|int The parent tag (or null if none)
-     * @param $options int The options (defaults 0)
-     */
-    protected function executeAddTag(string $name, $parent = null, int $options = 0) 
-    {
-        $parent_id = $this->GetParent($parent);
-        $id = DB::table('tags')->insertGetId(['name'=>$name,'parent_id'=>$parent_id,'options'=>$options]);
-        $tag = $this->loadTag($id);
-        $full_path = $tag->getFullPath();
-	    $fullpath = explode('.',$full_path);
-	    $is_fullpath = true;
-	    while (!empty($fullpath)) {
-	        DB::table('tagcache')->insert([
-	            'path_name'=>implode('.',$fullpath),
-	            'tag_id'=>$id,
-	            'is_fullpath'=>$is_fullpath
-	        ]);
-	        array_shift($fullpath);
-	        $is_fullpath = false;
-	    }
-	    return $id;
-    }
-    
-    protected function GetParent($parent) 
-    {
-        if (!empty($parent)) {
-            if (is_a($parent,Descriptor::class)) {
-                $name = $parent->name;
-                if (empty($name)) {
-                    return 0;           
-                }
-                $parent= $parent->name;
-            }
-            $parent_id = $this->searchTag($parent);
-            if (empty($parent_id)) {
-                return $this->addTagByString($parent);
-            } else {
-                return $parent_id->id;
-            }
-        } else {
-            return 0;
-        }        
-    }
-    
-    /**
-     * The passed data is an array
-     */
-     protected function addTagByArray(array $values) 
-     {
-            $this->executeAddTag($values['name'],isset($values['parent'])?$values['parent']:null);
-     }   
-    
-     /**
-      * The passed data is a Descriptor
-      */
-     protected function addTagByDescriptor(Descriptor $Descriptor) 
-     {
-        $this->executeAddTag($Descriptor->name,$Descriptor->assertHasKey('parent')?$Descriptor->parent:null);
-     }
-    
-     /**
-      * The passed data is a string
-      */
-     protected function addTagByString(string $tag) 
-     {
-         $tag_parts = explode('.',$tag);
-         $tag_name = array_pop($tag_parts);
-         return $this->executeAddTag($tag_name,implode('.',$tag_parts));
-     }
-    
-     /**
-      * The passed data is a Tag object
-      */
-     protected function addTagByObject(Tag $tag) 
-     {
-        $parent = $tag->getParent();
-        $this->executeAddTag($tag->getName(),$parent?$parent->getFullPath():null);
-     }
-    
-     protected function doSearchTag(string $name) 
-     {
-         return $this->prepareQuery()
-                ->join('tagcache', 'tagcache.tag_id','=','a.id')
-                ->where('tagcache.path_name',$name)->get();         
-     }
-     
-     /**
-      * Searches for a tag with the name $name and returns all found tag descriptors
-      */
-     public function searchTag(string $name) 
-     {
-         $query = $this->doSearchTag($name);
-         $return = [];
-         foreach ($query as $result) {
-             $return[] = $this->getQueryDescriptor($result);
-         }
-         switch (count($return)) {
-             case 0:
-                 return;
-             case 1:
-                 return $return[0];
-             default:
-                 return $return;
-         }
-     }
-     
-    /**
-     * Loads the given tag $tag.
-     * @param $tag string|int|Tag
-     * If $tag is a string then it searches for the fitting tag. 
-     * If $tag is a int then it searches for the tag with
-     * If $tag is a Tag object it returns $tag
-     * @return Tag
-     * @throws TagException if tag is not found or is not unique
-     */
-     public function loadTag($tag) 
-     {
-         if (is_a($tag,Tag::class)) {
+         if (count($this->query()->where('id',$id)->get())) {
+             $tag = new Tag();
+             $tag->load($id);
              return $tag;
          }
-         if (is_string($tag)) {
-             $result = $this->doSearchTag($tag);
-             if (count($result) == 0) {
-                 throw new TagException("Tag '$tag' not found.");
-             } else if (count($result) > 1) {
-                 throw new TagException("Tag '$tag' not unique.");
-             }
-             $tag_id = $result[0]->id;
-         } else if (is_int($tag)) {
-             $tag_id = $tag;
-         }
-         $tag = new Tag();
-         $tag->load($tag_id);
-         return $tag;
+         return null;
      }
-          
+     
+     /**
+      * Returns the TagQuery object for searching
+      * 
+      * @return BasicQuery
+      */
      public function query(): BasicQuery
      {
          return Storage::tagQuery();
--- tests/Feature/Managers/TagManagerTest.php
+++ tests/Feature/Managers/TagManagerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sunhill\ORM\Tests\Unit\Managers;
+namespace Sunhill\ORM\Tests\Feature\Managers;
 
 use Sunhill\ORM\Tests\DatabaseTestCase;
 use Sunhill\ORM\Managers\TagManager;
@@ -10,293 +10,79 @@ use Sunhill\ORM\Objects\Tag;
 use Illuminate\Support\Facades\DB;
 use Sunhill\Basic\Utils\Descriptor;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Sunhill\ORM\Objects\TagException;
 
 class TagManagerTest extends DatabaseTestCase
 {
-    // ========================== Test edit tags ==============================
+
     /**
-     * @group change
+     * @dataProvider SearchTagProvider
      */
-    public function testChangeTagName_TagChanged() {
-        
-        Tags::changeTag(3,['name'=>'NewTagC']);
-        $this->assertEquals('NewTagC',Tags::getTag(3)->name);
-        $this->assertEquals('TagB.NewTagC',Tags::getTag(3)->fullpath);
-    }
-    
-    // check if the tag cache was updated after the name of a tag changed
-    /**
-     * @group change
-     */
-    public function testChangeTagName_CacheUpdated() {
-        
-        Tags::changeTag(3,['name'=>'NewTagC']);    
-        $result = DB::table('tagcache')->where('tag_id',3)->get();
-        $this->assertEquals($result[0]->path_name,'NewTagC');
-    }
-    
-    // Change Parent of index tag
-    /**
-     * @group change
-     */
-    public function testChangeTagParent_TagChanged() {
-        
-        Tags::changeTag(3,['parent'=>'TagD']);   
-        $this->assertEquals('TagD.TagC',Tags::getTag(3)->fullpath);
+    public function testSearchTag($input, $expected_id, $multiple)
+    {
+        $result = Tags::searchTag($input);
+        if ($multiple && count($result) <2) {
+            $this->fail('Multiple results expected.');
+        }
+        if (is_null($expected_id)) {
+            $this->assertEquals(0, count($result));
+        } else {
+            $this->assertEquals($expected_id,$result[0]->id);
+        }
     }
 
-    // Check if tag cache was updated wheren parent of tag was changed
-    /**
-     * @group change
-     */
-    public function testChangeTagParent_CacheUpdated() {
-        
-        Tags::changeTag(3,['parent'=>'TagD']);
-        $result = DB::table('tagcache')->where('tag_id',3)->get();
-        $this->assertEquals($result[1]->path_name,'TagD.TagC');
-        $this->assertEquals($result[0]->path_name,'TagC');
-    }
-    
-    // Clear tags
-    public function testClearTags_CacheEmpty() {
-        Tags::clearTags();
-        $result = DB::table('tagcache')->get();
-        $this->assertTrue($result->isEmpty());
-    }
-    
-    public function testClearTags_ReferenceEmpty() {
-        Tags::clearTags();
-        $result = DB::table('tagobjectassigns')->get();
-        $this->assertTrue($result->isEmpty());
-    }
-    
-    public function testClearTags_TagsEmpty() {
-        Tags::clearTags();
-        $result = DB::table('tags')->get();
-        $this->assertTrue($result->isEmpty());
-    }
-    
-    // delete tag index
-    /**
-     * @group delete
-     */
-    public function testDeleteTag_TagDeleted() {
-        
-        Tags::deleteTag(3);
-        $this->assertNull(Tags::getTag(3));
-    }
-    
-    // Check if tag cache was updated wheren tag was deleted
-    /**
-     * @group delete
-     */
-    public function testDeleteTag_CacheUpdated() {
-        
-        Tags::deleteTag(3);
-        $result = DB::table('tagcache')->where('tag_id',3)->get();
-        $this->assertTrue($result->isEmpty());
-    }
-    
-    // Check if tag object association was updated wheren tag was deleted
-    /**
-     * @group delete
-     */
-    public function testDeleteTag_AssociationsUpdated() {
-        
-        Tags::deleteTag(3);
-        $result = DB::table('tagobjectassigns')->where('tag_id',3)->get();
-        $this->assertTrue($result->isEmpty());
-    }
-    
-    /**
-     * @dataProvider GetTagIDProvider
-     * @group add
-     */
-    public function test_get_tag_id($parent,$expect) {
-        $test = new TagManager();
-        if (is_callable($parent)) {
-            $parent = $parent();
-        }
-        $tag = $this->callProtectedMethod($test,'getTagID',[$parent]);
-        $this->assertEquals($expect,$tag);
-    }
-    
-    public function GetTagIDProvider() {
+    public function SearchTagProvider()
+    {
         return [
-            [null,0],
-            [1,1],
-            ['TagA',1],
-            ['TagB.TagC',3],
-            [function(){ return Tags::loadTag(3); },3]
+            ['TagA',1,false],
+            ['TagC',3,false],
+            ['TagB.TagC',3,false],
+            ['TagE',5,true],
+            ['NonExisting',null,false]
+        ];
+    }
+
+    /**
+     * @dataProvider GetTagProvider
+     * @param unknown $input
+     * @param unknown $expected_id
+     * @param unknown $exception
+     */
+    public function testGetTag($input, $expected_id, $exception)
+    {
+        if  (!is_null($exception)) {
+            $this->expectException($exception);
+        }
+        if (is_callable($input)) {
+            $input = $input();
+        }
+        $this->assertEquals($expected_id,Tags::getTag($input)->getID());
+    }
+    
+    public function GetTagProvider()
+    {
+        return [
+            [1,1,null],
+            [function() { $result = new Tag(); $result->load(2); return $result; }, 2, null ],
+            ['TagA',1,null],
+            ['TagG',7,null],
+            ['TagE',0,TagException::class],
+            ['Nonexisting',0,TagException::class]
         ];
     }
     
-    /**
-     * @group add
-     */
-    public function testExecuteAddTag_TagAdded() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'executeAddTag',['Test','TagA']);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertEquals(1,$result->parent_id);                
-    }
-    
-    /**
-     * @group add
-     */
-    public function testExecuteAddTag_TagAddedNoParent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'executeAddTag',['Test',null]);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertEquals(0,$result->parent_id);                
-    }
-    
-    /**
-     * @group add
-     */
-    public function testExecuteAddTag_TagCacheAdded() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'executeAddTag',['Test','TagA']);
-        $result = DB::table('tagcache')->where('path_name','TagA.Test')->get();
-        $this->assertTrue($result->count()>0);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withString_no_parent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'addTagByString',['Test']);
-        $result = DB::table('tags')->where('name','Test')->get();
-        $this->assertTrue($result->count()>0);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withString_parent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'addTagByString',['TagA.Test']);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertEquals(1,$result->parent_id);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withString_missingparent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'addTagByString',['TagZ.Test']);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertTrue($result->parent_id>1);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withArray_no_parent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'addTagByString',['Test']);
-        $result = DB::table('tags')->where('name','Test')->get();
-        $this->assertTrue($result->count()>0);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withArray_parent() {
-        $test = new TagManager();
-        $this->callProtectedMethod($test,'addTagByString',['TagA.Test']);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertEquals(1,$result->parent_id);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withDescriptor_no_parent() {
-        $test = new TagManager();
-        $Descriptor = new Descriptor();
-        $Descriptor->name = 'Test';
-        $Descriptor->parent = 'TagA';
-        $this->callProtectedMethod($test,'addTagByDescriptor',[$Descriptor]);
-        $result = DB::table('tags')->where('name','Test')->get();
-        $this->assertTrue($result->count()>0);
-    }
-    
-    /**
-     * @group add
-     */
-    public function testAddTag_withDescriptor_parent() {
-        $test = new TagManager();
-        $Descriptor = new Descriptor();
-        $Descriptor->name = 'Test';
-        $Descriptor->parent = 'TagA';
-        $this->callProtectedMethod($test,'addTagByDescriptor',[$Descriptor]);
-        $result = DB::table('tags')->where('name','Test')->first();
-        $this->assertEquals(1,$result->parent_id);
-    }
-    
-    /**
-     * @group add
-     * @dataProvider AddTagProvider
-     */
-    public function testAddTag($tag,$expect) {        
-        if (is_callable($tag)) {
-            $tag = $tag();
-        }
-        Tags::addTag($tag);
-        $result = DB::table('tags')->where('name',$expect)->get();
-        $this->assertEquals($expect,$result[0]->name);
-    }
-
-    public function AddTagProvider() {
-        return [
-            ['Test','Test'],
-            ['TagA.Test','Test'],
-            [['name'=>'Test'],'Test'],
-            [function() { $Descriptor = new Descriptor(); $Descriptor->name = 'Test'; return $Descriptor; },'Test'],
-            [function() { $tag = new Tag(); $tag->setName('Test'); return $tag; },'Test'],
-        ];
-    }                                                                
-                                                                
-    /**
-     * @dataProvider QueryProvider
-     * @group query
-     */
-    public function testQuery($callback, $modifier, $expect)
+    public function testLoadTagPass()
     {
-        $query = Tags::query();
-        $result = $callback($query);
-        
-        if (is_callable($modifier)) {
-            $result = $modifier($result);
-        }
-        $this->assertEquals($expect, $result);
+        $this->assertEquals(2,Tags::loadTag(2)->getID());
     }
     
-    public function QueryProvider()
+    public function testLoadTagFail()
     {
-        return [
-            [function($query) { return $query->count(); }, null, 9],
-            [function($query) { return $query->first(); }, function($value){ return $value->name; }, 'TagA'],            
-            [function($query) { return $query->offset(5)->first(); }, function($value) { return $value->name; }, 'TagF'],
-            [function($query) { return $query->orderBy('name')->offset(5)->first(); }, function($value) { return $value->name; }, 'TagE'],
-            [function($query) { return $query->where('name','TagC')->first(); }, function($value){ return $value->name; }, 'TagC'],            
-            [function($query) { return $query->where('parent_id','<>',0)->first(); }, function($value){ return $value->name; }, 'TagC'],
-            [function($query) { return $query->where('parent','=','TagF')->get(); }, function($value){ return $value[0]->name; }, 'TagG'],
-            [function($query) { return $query->where('is assigned')->get(); }, function($value){ return $value[0]->name; }, 'TagA'],
-            [function($query) { return $query->where('not assigned')->get(); }, function($value){ return $value[2]->name; }, 'TagZ'],
-            ];
+        $this->assertEquals(null,Tags::loadTag(1000));
     }
- 
-    /**
-     * @group gettags
-     */
-    public function testGetTags()
+    
+    public function testTagQuery()
     {
-        $query = Tags::query()->getTags();
-        $this->assertEquals(9,count($query));
-        $this->assertEquals(1,$query[0]->getID());
+        $this->assertEquals(1, Tags::query()->where('name','TagA')->first()->id);
     }
 }
