395cc8088183a10a4cfefa03e07da7adc9282b11
Klaus Dimde <klaus@dimde.info>
2023-06-04 00:24:37 +0200

    Add another attribute for testing unassigned ones

--- tests/Database/Seeders/AttributesTableSeeder.php
+++ tests/Database/Seeders/AttributesTableSeeder.php
@@ -9,14 +9,15 @@ class AttributesTableSeeder extends Seeder {
 	public function run() {
 	    DB::table('attributes')->truncate();
 	    DB::table('attributes')->insert([
-	        ['id'=>1,'name'=>'int_attribute','type'=>'integer','allowedobjects'=>"dummy"],
-	        ['id'=>2,'name'=>'attribute1','type'=>'integer','allowedobjects'=>"testparent"],
-	        ['id'=>3,'name'=>'attribute2','type'=>'integer','allowedobjects'=>"testparent"],
-	        ['id'=>4,'name'=>'general_attribute','type'=>'integer','allowedobjects'=>"object"],
-	        ['id'=>5,'name'=>'char_attribute','type'=>'string','allowedobjects'=>"dummy"],
-	        ['id'=>6,'name'=>'float_attribute','type'=>'float','allowedobjects'=>"dummy"],
-	        ['id'=>7,'name'=>'text_attribute','type'=>'text','allowedobjects'=>"dummy"],
-	        ['id'=>8,'name'=>'child_attribute','type'=>'integer','allowedobjects'=>"dummychild"],
+	        ['id'=>1,'name'=>'int_attribute','type'=>'integer','allowed_classes'=>"|dummy|"],
+	        ['id'=>2,'name'=>'attribute1','type'=>'integer','allowed_classes'=>"|testparent|"],
+	        ['id'=>3,'name'=>'attribute2','type'=>'integer','allowed_classes'=>"|testparent|"],
+	        ['id'=>4,'name'=>'general_attribute','type'=>'integer','allowed_classes'=>"|object|"],
+	        ['id'=>5,'name'=>'char_attribute','type'=>'string','allowed_classes'=>"|dummy|"],
+	        ['id'=>6,'name'=>'float_attribute','type'=>'float','allowed_classes'=>"|dummy|"],
+	        ['id'=>7,'name'=>'text_attribute','type'=>'text','allowed_classes'=>"|dummy|"],
+	        ['id'=>8,'name'=>'child_attribute','type'=>'integer','allowed_classes'=>"|dummychild|"],
+	        ['id'=>9,'name'=>'empty','type'=>'integer','allowed_classes'=>"|dummychild|"],
 	    ]);
 	}
 }
