7bc8a65e7b6b47c86e1118fe20fb770c8d6b8cb0
Klaus Dimde <klaus@dimde.info>
2023-06-14 21:58:11 +0200

    More validation tests

--- tests/Unit/Properties/PropertyValidateTest.php
+++ tests/Unit/Properties/PropertyValidateTest.php
@@ -2,16 +2,22 @@
 
 use Sunhill\ORM\Tests\TestCase;
 use Sunhill\ORM\Properties\Property;
+use Sunhill\ORM\Properties\PropertyArray;
 use Sunhill\ORM\Properties\PropertyBoolean;
 use Sunhill\ORM\Properties\PropertyDate;
+use Sunhill\ORM\Properties\PropertyDatetime;
+use Sunhill\ORM\Properties\PropertyEnum;
 use Sunhill\ORM\Properties\PropertyFloat;
 use Sunhill\ORM\Properties\PropertyInteger;
 use Sunhill\ORM\Properties\PropertyObject;
+use Sunhill\ORM\Properties\PropertyCollection;
 use Sunhill\ORM\Facades\Classes;
 use Sunhill\ORM\Tests\Testobjects\Dummy;
 use Sunhill\ORM\Tests\Testobjects\DummyChild;
 use Sunhill\ORM\Properties\PropertyVarchar;
 use Sunhill\ORM\Tests\Testobjects\TestParent;
+use Sunhill\ORM\Tests\Testobjects\DummyCollection;
+use Sunhill\ORM\Tests\Testobjects\ComplexCollection;
 
 class PropertyValidateTest extends TestCase
 {
@@ -44,24 +50,24 @@ class PropertyValidateTest extends TestCase
     public function ValidateProvider()
     {
         return [
-           // ========================== Integer ===============================
-            [PropertyInteger::class, null, 1, true],
-            [PropertyInteger::class, null, 1.1, false],
-            [PropertyInteger::class, null, 'A', false],
-            [PropertyInteger::class, null, '1', true],
-
-            // ========================== Float ===============================
-            [PropertyFloat::class, null, 1, true],
-            [PropertyFloat::class, null, 1.1, true],
-            [PropertyFloat::class, null, "1", true],
-            [PropertyFloat::class, null, "1.1", true],
-            [PropertyFloat::class, null, "A", false],
-            [PropertyFloat::class, null, "1.1.1", false],
-
-            // ========================== Varchar ===============================
-            [PropertyVarchar::class, null, 1, true],
-            [PropertyVarchar::class, null, 1.1, true],
-            [PropertyVarchar::class, null, "1", true],
+            // =========================== Array ================================
+            [PropertyArray::class, function(&$property) {
+                $property->setElementType(PropertyInteger::class);
+            }, 44, true],
+            [PropertyArray::class, function(&$property) {
+                $property->setElementType(PropertyInteger::class);
+            }, 'ABC', false],
+            [PropertyArray::class, function(&$property) {
+                $property->setElementType(PropertyObject::class)->setAllowedClasses(Dummy::class);
+            }, function() {
+                return new Dummy();
+            }, true],
+            [PropertyArray::class, function(&$property) {
+                $property->setElementType(PropertyObject::class)->setAllowedClasses(Dummy::class);
+            }, function() {
+                return new TestParent();
+            }, false],
+            
             
             // ========================== Boolean ===============================
             [PropertyBoolean::class, null, 'Y', true],
@@ -77,6 +83,18 @@ class PropertyValidateTest extends TestCase
             [PropertyBoolean::class, null, 0, true],
             [PropertyBoolean::class, null, 10, true],
             
+            // ========================= Collection =============================
+            [PropertyCollection::class, function(&$property) {
+                $property->setAllowedClass(DummyCollection::class);                
+            }, function() {
+                return new DummyCollection();
+            }, true],
+            [PropertyCollection::class, function(&$property) {
+                $property->setAllowedClass(DummyCollection::class);
+            }, function() {
+                return new ComplexCollection();
+            }, false],
+            
             // =========================== Date ================================
             [PropertyDate::class, null, '1.2.2018', true],
             [PropertyDate::class, null, '2018-2-1', true],
@@ -84,9 +102,34 @@ class PropertyValidateTest extends TestCase
             [PropertyDate::class, null, '2018-2', true],
             [PropertyDate::class, null, '2.3.', true],
             [PropertyDate::class, null, '2018', true],
-            [PropertyDate::class, null, false, false],            
+            [PropertyDate::class, null, false, false],
             [PropertyDate::class, null, 'ABC', false],
             [PropertyDate::class, null, '', false],
+            
+            // ========================= DateTime ===============================
+            [PropertyDatetime::class, null, '2018-02-01 11:11:11', true],
+            
+            // =========================== Enum =================================
+            [PropertyEnum::class, function(&$property) {
+                $property->setEnumValues(['TestA','TestB']);
+            }, 'TestA', true],
+            [PropertyEnum::class, function(&$property) {
+                $property->setEnumValues(['TestA','TestB']);                
+            }, 'NonExisting', false],
+            
+            // ========================== Float ===============================
+            [PropertyFloat::class, null, 1, true],
+            [PropertyFloat::class, null, 1.1, true],
+            [PropertyFloat::class, null, "1", true],
+            [PropertyFloat::class, null, "1.1", true],
+            [PropertyFloat::class, null, "A", false],
+            [PropertyFloat::class, null, "1.1.1", false],
+            
+            // ========================== Integer ===============================
+            [PropertyInteger::class, null, 1, true],
+            [PropertyInteger::class, null, 1.1, false],
+            [PropertyInteger::class, null, 'A', false],
+            [PropertyInteger::class, null, '1', true],
             
             // ========================== Object ===============================
             [
@@ -143,7 +186,15 @@ class PropertyValidateTest extends TestCase
                     return new Dummy();
                 }, false
             ],
-          ];
+            // =========================== Text =================================
+            // =========================== Time =================================
+            // ========================== Varchar ===============================
+                [PropertyVarchar::class, null, 1, true],
+                [PropertyVarchar::class, null, 1.1, true],
+                [PropertyVarchar::class, null, "1", true],
+                
+                
+         ];
     }
     
     /**
@@ -175,4 +226,5 @@ class PropertyValidateTest extends TestCase
             [PropertyVarchar::class, function($property) { $property->setMaxLen(2); }, "ABCD", "AB"],
         ];
     }
+    
 }
