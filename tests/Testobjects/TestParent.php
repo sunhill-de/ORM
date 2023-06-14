af277dcc2e0a3c398dcfd16628a2934ab23839e4
Klaus Dimde <klaus@dimde.info>
2023-06-14 21:57:41 +0200

    Added remaining new properties

--- tests/Testobjects/TestParent.php
+++ tests/Testobjects/TestParent.php
@@ -13,6 +13,8 @@ namespace Sunhill\ORM\Tests\Testobjects;
 
 use Sunhill\ORM\Objects\ORMObject;
 use Sunhill\ORM\Objects\PropertyList;
+use Sunhill\ORM\Properties\PropertyObject;
+use Sunhill\ORM\Properties\PropertyVarchar;
 
 class TestParent extends ORMObject 
 {
@@ -31,10 +33,15 @@ class TestParent extends ORMObject
 		$list->date('parentdate')->searchable();
 		$list->time('parenttime')->searchable();
 		$list->enum('parentenum')->setValues(['testA','testB','testC'])->searchable();
+		$list->boolean('parentbool');
 		$list->object('parentobject')->setAllowedClasses(['dummy'])->setDefault(null)->searchable();
-		$list->arrayofstrings('parentsarray')->searchable();
-		$list->arrayOfObjects('parentoarray')->setAllowedClasses(['dummy'])->searchable();
+		$list->array('parentsarray')->setElementType(PropertyVarchar::class)->searchable();
+		$list->array('parentoarray')->setElementType(PropertyObject::class)->setAllowedClasses(['dummy'])->searchable();
 		$list->calculated('parentcalc')->searchable();
+		$list->collection('parentcollection')->setAllowedClasses(DummyCollection::class)->searchable();
+		$list->keyfield('parentkeyfield',':parentchar (:parentint)')->searchable();
+		$list->map('parentmap')->setElementType(PropertyVarchar::class)->searchable();
+		$list->externalReference('parent_external', 'external', 'id_field')->setInternalKey('id');
 		$list->integer('nosearch')->setDefault(1);
 	}
 	
@@ -47,8 +54,6 @@ class TestParent extends ORMObject
 	{
 	    static::addInfo('name', 'testparent');
 	    static::addInfo('table', 'testparents');
-	    static::addInfo('name_s', 'test parent');
-	    static::addInfo('name_p', 'test parents');
 	    static::addInfo('description', 'Another test class. A class with all avaiable properties');
 	    static::addInfo('options', 0);
 	}
