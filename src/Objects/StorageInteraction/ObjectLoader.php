2361176702c139230f26172fff4a18c547989013
Klaus Dimde <klaus@dimde.info>
2023-06-24 21:23:46 +0200

    Handles tags and attributes

--- src/Objects/StorageInteraction/ObjectLoader.php
+++ src/Objects/StorageInteraction/ObjectLoader.php
@@ -17,7 +17,36 @@
 
 namespace Sunhill\ORM\Objects\StorageInteraction;
 
+use Sunhill\ORM\Facades\Tags;
+
 class ObjectLoader extends CollectionLoader
 {
+   
+    public function handlePropertyTags($property)
+    {
+        if (!$this->storage->hasEntity('tags')) {
+            return;
+        }
+        $tags = $this->storage->getEntity('tags')->getValue();
+        if (empty($tags)) {
+            return;
+        }
+        foreach ($tags as $tag) {
+            $tag = Tags::loadTag($tag);
+            $this->collection->tags->stick($tag);
+        }
+    }
+    
+    protected function additionalRun()
+    {
+        if (!$this->storage->hasEntity('attributes')) {
+            return;
+        }
+        foreach ($this->storage->getEntity('attributes') as $attribute) {
+            $attribute_obj = $this->collection->dynamicAddProperty($attribute->name, $attribute->type);
+            $attribute_obj->setAttributeID($attribute->attribute_id);
+            $attribute_obj->setValue($attribute->value);            
+        }
+    }
     
 }
