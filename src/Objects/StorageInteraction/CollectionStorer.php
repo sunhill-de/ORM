c5120fe0a776f9473b9caaa6231520695adbc1c8
Klaus Dimde <klaus@dimde.info>
2023-06-25 22:34:25 +0200

    Implemented collection storage

--- src/Objects/StorageInteraction/CollectionStorer.php
+++ src/Objects/StorageInteraction/CollectionStorer.php
@@ -19,38 +19,80 @@ namespace Sunhill\ORM\Objects\StorageInteraction;
 
 use Sunhill\ORM\Properties\PropertyObject;
 use Sunhill\ORM\Facades\Objects;
+use Sunhill\ORM\Properties\PropertyBoolean;
+use Sunhill\ORM\Properties\PropertyInteger;
+use Sunhill\ORM\Properties\PropertyCalculated;
+use Sunhill\ORM\Properties\PropertyDate;
+use Sunhill\ORM\Properties\PropertyDatetime;
+use Sunhill\ORM\Properties\PropertyEnum;
+use Sunhill\ORM\Properties\PropertyFloat;
+use Sunhill\ORM\Properties\PropertyTime;
+use Sunhill\ORM\Properties\PropertyVarchar;
+use Sunhill\ORM\Properties\PropertyText;
+use Sunhill\ORM\Properties\PropertyArray;
+use Sunhill\ORM\Properties\PropertyMap;
 
 class CollectionStorer extends StorageInteractionBase
 {
         
+    protected function copyValue($property, $value)
+    {
+        return $this->storage->createEntity($property->getName(), $this->collection->getInfo('table'))
+            ->setValue($value);        
+    }
+
+    protected function copyProperty($property)
+    {
+        return $this->copyValue($property, $property->getValue());    
+    }
+    
+    protected function copyArrayOrMap($property)
+    {
+        $values = [];
+        foreach ($property->getValue() as $key => $element) {
+            if ($property->getElementType() == PropertyObject::class) {
+                $values[$key] = $element->getID();                
+            } else {
+                $values[$key] = $element;
+            }
+        }
+        return $this->storage->createEntity($property->getName(), $this->collection->getInfo('table'))
+        ->setValue($values);        
+    }
     
     public function handlePropertyArray($property)
     {
+        $this->copyArrayOrMap($property)->setType(PropertyArray::class)->setElementType($property->getElementType());
     }
     
     public function handlePropertyBoolean($property)
     {   
+        $this->copyProperty($property)->setType(PropertyBoolean::class);
     }
     
     public function handlePropertyCalculated($property)
     {
+        $this->copyProperty($property)->setType(PropertyCalculated::class);
     }
     
     public function handlePropertyCollection($property)
     {
-        
+        $this->copyValue($property, $property->getID());        
     }
     
     public function handlePropertyDate($property)
     {
+        $this->copyProperty($property)->setType(PropertyDate::class);
     }
     
     public function handlePropertyDateTime($property)
     {
+        $this->copyProperty($property)->setType(PropertyDatetime::class);
     }
     
     public function handlePropertyEnum($property)
     {        
+        $this->copyProperty($property)->setType(PropertyEnum::class);        
     }
     
     public function handlePropertyExternalReference($property)
@@ -59,6 +101,7 @@ class CollectionStorer extends StorageInteractionBase
     
     public function handlePropertyFloat($property)
     {
+        $this->copyProperty($property)->setType(PropertyFloat::class);        
     }
     
     public function handlePropertyInformation($property)
@@ -67,6 +110,7 @@ class CollectionStorer extends StorageInteractionBase
     
     public function handlePropertyInteger($property)
     {
+        $this->copyProperty($property)->setType(PropertyInteger::class);
     }
     
     public function handlePropertyKeyfield($property)
@@ -75,26 +119,37 @@ class CollectionStorer extends StorageInteractionBase
     
     public function handlePropertyMap($property)
     {
+        $this->copyArrayOrMap($property)->setType(PropertyMap::class)->setElementType($property->getElementType());;
     }
     
     public function handlePropertyObject($property)
-    {
+    {        
+        $this->copyValue($property, $property->getValue()->getID())->setType(PropertyObject::class); 
     }
     
     public function handlePropertyTags($property)
     {
+        $values = [];
+        foreach ($this->tags as $tag) {
+            $values[] = $tag->getID();
+        }
+        $this->storage->createEntity($property->getName(), $this->collection->getInfo('table'))
+        ->setValue($value);
     }
     
     public function handlePropertyText($property)
     {
+        $this->copyProperty($property)->setType(PropertyText::class);        
     }
     
     public function handlePropertyTime($property)
     {
+        $this->copyProperty($property)->setType(PropertyTime::class);
     }
     
     public function handlePropertyVarchar($property)
     {
+        $this->copyProperty($property)->setType(PropertyVarchar::class);        
     }
     
     protected function additionalRun()
