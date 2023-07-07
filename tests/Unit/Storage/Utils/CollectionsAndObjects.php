3a0dd1a597e11dfa8ca99ab60a5931f6ef9b110a
Klaus Dimde <klaus@dimde.info>
2023-07-07 10:12:38 +0200

    Added some utilities

--- tests/Unit/Storage/Utils/CollectionsAndObjects.php
+++ tests/Unit/Storage/Utils/CollectionsAndObjects.php
@@ -4,6 +4,8 @@ namespace Sunhill\ORM\Tests\Unit\Storage\Utils;
 
 use Sunhill\ORM\Tests\Testobjects\Dummy;
 use Sunhill\ORM\Tests\Testobjects\DummyCollection;
+use Sunhill\ORM\Objects\Tag;
+use Sunhill\ORM\Tests\Testobjects\ComplexCollection;
 
 trait CollectionsAndObjects 
 {
@@ -21,5 +23,24 @@ trait CollectionsAndObjects
         $test->setID($id);
         return $test;
     }
+    
+    protected function getComplexCollection($id)
+    {
+        $test = new ComplexCollection();
+        $test->setID($id);
+        return $test;        
+    }
+    
+    protected function getTag($id)
+    {
+        $test = new Tag();
+        $test->load($id);
+        return $test;
+    }
+    
+    protected function getAttribute($id, $name, $value, $type)
+    {
         
+    }
+    
 }
