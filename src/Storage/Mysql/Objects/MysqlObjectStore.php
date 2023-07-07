55a0b0862ffbdde84182a1ebbca70ae0212b6860
Klaus Dimde <klaus@dimde.info>
2023-07-07 12:30:54 +0200

    Implemented store of objects

--- src/Storage/Mysql/Objects/MysqlObjectStore.php
+++ src/Storage/Mysql/Objects/MysqlObjectStore.php
@@ -2,19 +2,51 @@
 
 namespace Sunhill\ORM\Storage\Mysql\Objects;
 
+use Illuminate\Support\Facades\DB;
 use Sunhill\ORM\Storage\Mysql\Collections\MysqlCollectionStore;
+use Sunhill\ORM\Objects\ORMObject;
 
 class MysqlObjectStore extends MysqlCollectionStore
 {
 
-    protected function storeAttributes()
-    {
-    }
-    
     public function run()
     {
+        $this->prerunTables();
         parent::run();
-        $this->storeAttributes();
+    }
+    
+    protected function getClassList($target)
+    {
+        $result = [];
+        $target = $target::class;
+        while ($target !== ORMObject::class) {
+            $result[] = $target;
+            $target = get_parent_class($target);
+        }
+        return $result;
+    }
+    
+    protected function prerunTables()
+    {
+        $list = $this->getClassList($this->collection); 
+        foreach ($list as $parent) {
+            $this->tables[$parent::getInfo('table')] = [];
+        }
+    }
+    
+    protected function handleAttribute($property)
+    {
+        $this->addEntry('attr_'.$property->name,'value',$property->value);
+        $this->addEntryRecord('attributeobjectassigns', ['attribute_id'=>$property->attribute_id]);
+    }
+    
+    protected function storeMainTable()
+    {
+        $this->tables['objects']['classname'] = $this->collection::getInfo('name');
+        $this->storeTable('objects', $this->tables['objects']);
+        $this->id = DB::getPdo()->lastInsertId();
+        $this->collection->setID($this->id);
+        unset($this->tables['objects']);
     }
     
     public function handlePropertyInformation($property)
@@ -23,6 +55,9 @@ class MysqlObjectStore extends MysqlCollectionStore
     
     public function handlePropertyTags($property)
     {
+        foreach ($property->value as $tag) {
+            $this->addEntryRecord('tagobjectassigns',['tag_id'=>$tag->getID()]);            
+        }
     }
     
 }
