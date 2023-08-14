e86f469340e82deda78ff94ba812f7ff383fa9bc
Klaus Dimde <klaus@dimde.info>
2023-08-14 12:29:01 +0200

    Implemented the Where Parser

---
+++ src/Storage/Mysql/WhereParser.php
@@ -0,0 +1,414 @@
+<?php
+
+/**
+ * @file WhereParser.php
+ * Utility to parse the parameter passed to a collection/object query where function
+ * @author Klaus Dimde
+ * ---------------------------------------------------------------------------------------------------------
+ * Lang en
+ * Reviewstatus: 2023-08-13
+ * Localization: not necessary
+ * Documentation: complete
+ * Tests: tests/Unit/Managers/ManagerClassesTest.php
+ * Coverage: 98,8% (2023-03-23)
+ */
+namespace Sunhill\ORM\Storage\Mysql;
+
+use Sunhill\ORM\Query\TooManyWhereParametersException;
+use Sunhill\ORM\Facades\Attributes;
+use Sunhill\ORM\Query\UnknownFieldException;
+use Sunhill\ORM\Query\WrongTypeException;
+use Sunhill\ORM\Properties\Property;
+use Sunhill\ORM\Properties\PropertyArray;
+use Sunhill\ORM\Properties\PropertyBoolean;
+use Sunhill\ORM\Properties\PropertyCalculated;
+use Sunhill\ORM\Properties\PropertyCollection;
+use Sunhill\ORM\Properties\PropertyDate;
+use Sunhill\ORM\Properties\PropertyDatetime;
+use Sunhill\ORM\Properties\PropertyEnum;
+use Sunhill\ORM\Properties\PropertyFloat;
+use Sunhill\ORM\Properties\PropertyInformation;
+use Sunhill\ORM\Properties\PropertyExternalReference;
+use Sunhill\ORM\Properties\PropertyInteger;
+use Sunhill\ORM\Properties\PropertyKeyfield;
+use Sunhill\ORM\Properties\PropertyMap;
+use Sunhill\ORM\Properties\PropertyObject;
+use Sunhill\ORM\Properties\PropertyTags;
+use Sunhill\ORM\Properties\PropertyText;
+use Sunhill\ORM\Properties\PropertyTime;
+use Sunhill\ORM\Properties\PropertyVarchar;
+use Sunhill\ORM\Query\NotAllowedRelationException;
+
+class WhereParser
+{
+    
+    protected $tranlate_relation = [
+        '=='=>'=',
+        '!='=>'<>',
+        'has'=>'any of',
+    ];
+    
+    protected $relations = [
+        '='=>[
+            'arguments'=>'binary',
+            'type'=>['scalar','string','primitive','array','map','object','collection'],
+            'value'=>'native',
+            'function'=>'handleWhereSimple'
+        ],
+        '<>'=>[
+            'arguments'=>'binary',
+            'type'=>['scalar','string','primitive','array','map','object','collection'],
+            'value'=>'native'
+        ],
+        
+        '<'=>['arguments'=>'binary','type'=>['scalar','string'],'value'=>'native'],
+        '>'=>['arguments'=>'binary','type'=>['scalar','string'],'value'=>'native'],
+        '<='=>['arguments'=>'binary','type'=>['scalar','string'],'value'=>'native'],
+        '>='=>['arguments'=>'binary','type'=>['scalar','string'],'value'=>'native'],
+        
+        'in'=>[
+            'arguments'=>'binary',
+            'type'=>['scalar','string','primitive','object','collection'],
+            'value'=>'array'
+        ],
+        'null'=>[
+            'arguments'=>'unary',
+            'type'=>['scalar','string','primitive','object','collection'],
+            'value'=>'none'
+        ],
+        
+        'contains'=>['arguments'=>'binary','type'=>['string','array','map','tags'],'value'=>'native'],
+        'begins with'=>['arguments'=>'binary','type'=>['string'],'value'=>'native'],
+        'ends with'=>['arguments'=>'binary','type'=>['string'],'value'=>'native'],
+        
+        'all of'=>['arguments'=>'binary','type'=>['array','map','tag'],'value'=>'array'],
+        'any of'=>['arguments'=>'binary','type'=>['array','map','tag'],'value'=>'array'],
+        'none of'=>['arguments'=>'binary','type'=>['array','map','tag'],'value'=>'array'],
+        'all keys of'=>['arguments'=>'binary','type'=>['array','map','tag'],'value'=>'array'],
+        'any key of'=>['arguments'=>'binary','type'=>['map'],'value'=>'array'],
+        'none key of'=>['arguments'=>'binary','type'=>['map'],'value'=>'array'],
+        
+        'has associations'=>['arguments'=>'none'],
+        'is associated'=>['arguments'=>'none'],
+        'has attributes'=>['arguments'=>'none'],
+        'has tags'=>['arguments'=>'none']
+    ];
+    
+    protected $class;
+    
+    protected $connection;
+    
+    protected $key;
+    
+    protected $relation;
+    
+    protected $value;
+    
+    public function setClass($class)
+    {
+        $this->class = $class;
+        return $this;
+    }
+    
+    public function setConnection($connection)
+    {
+        $this->connection = $connection;
+        return $this;
+    }
+    
+    public function setKey($key)
+    {
+        $this->key = $key;
+        return $this;
+    }
+    
+    public function setRelation($relation)
+    {
+        $this->relation = $relation;
+        return $this;
+    }
+    
+    public function setValue($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+    
+    /**
+     * Check for unary conditions (the ones that don't expect an additional parameter)
+     * @throws TooManyWhereParametersException
+     * @return boolean|string[]|NULL[]
+     */
+    protected function checkUnaryRelation()
+    {
+        if ((!isset($this->relations[$this->key])) || ($this->relations[$this->key]['arguments'] <> 'none')) {
+            return false;
+        }
+        if (!is_null($this->relation) || (!is_null($this->value))) {
+            throw new TooManyWhereParametersException("With the relation '".$this->key."' no other parameters are allowed.");
+        }
+        switch ($this->key) {
+            case 'has associations':
+                return ['handleWhereHasAssociations', $this->connection];
+                break;
+            case 'is associated':
+                return ['handleWhereIsAssociated', $this->connection];
+                break;
+            case 'has tags':
+                return ['handleWhereHasTags', $this->connection];
+                break;
+            case 'has attributes':
+                return ['handleWhereHasAttributes', $this->connection];
+                break;
+            default:
+                return false;
+        }
+    }
+    
+    /**
+     * This checks for specials keys that are no properties
+     */
+    protected function checkSpecialKey()
+    {
+        
+    }
+    
+    protected function checkKey()
+    {
+        $class = $this->class;
+        if ($class::definesProperty($this->key)) {
+            return $class::getPropertyObject($this->key);
+        }
+        return false;
+    }
+    
+    protected function tryToTranslateRelation()
+    {
+        $this->relation = strtolower($this->relation);
+        if (isset($this->tranlate_relation[$this->relation])) {
+            $this->relation = $this->tranlate_relation[$this->relation];
+        }
+    }
+    
+    protected function handleAttribute(\StdClass $attribute)
+    {
+        if (is_null($this->value)) { // Note: There can't be an attribute with the value null (this removes an attribute)
+            $this->value = $this->relation;
+            $this->relation = '=';
+        } else {
+            $this->tryToTranslateRelation();
+        }
+        if ($attribute->type == 'string') {
+            if (!in_array('scalar',$this->relations[$this->relation]['type']) && !in_array('string',$this->relations[$this->relation]['type'])) {
+                throw new \Sunhill\ORM\Query\NotAllowedRelationException();                
+            }
+        } else {
+            if (!in_array('scalar',$this->relations[$this->relation]['type'])) {
+                throw new \Sunhill\ORM\Query\NotAllowedRelationException();
+            }
+        }
+        if ($this->relation == 'in') {
+            if (!is_array($this->value)) {
+                throw new WrongTypeException("The relation 'in' needs an array");
+            }
+            return ['handleWhereAttributeIn', $this->connection, $attribute, $this->value];            
+        } else {
+            return ['handleWhereAttributeSimple', $this->connection, $attribute, $this->relation, $this->value];
+        }
+    }
+    
+    protected function checkAttribute()
+    {
+        if (empty($all_attributes = Attributes::getAvaiableAttributesForClass($this->class))) {
+            return false;   
+        }
+        foreach ($all_attributes as $attribute) {
+            if ($attribute->name == $this->key) {
+                return $this->handleAttribute($attribute);
+            }
+            return false;
+        }
+    }
+    
+    protected function getPropertyType(Property $property): string
+    {
+        switch ($property::class) {
+            case PropertyArray::class:
+                return 'array';
+            case PropertyBoolean::class:
+            case PropertyEnum::class:
+                return 'primitive';
+            case PropertyCalculated::class:
+                return 'string';
+            case PropertyCollection::class:
+                return 'collection';
+            case PropertyDate::class:
+                return 'date';
+            case PropertyDatetime::class:
+                return 'datetime';
+            case PropertyExternalReference::class:
+                return 'external';
+            case PropertyFloat::class:
+            case PropertyInteger::class:
+                return 'scalar';
+            case PropertyInformation::class:
+                return 'information';
+            case PropertyMap::class:
+                return 'map';
+            case PropertyObject::class:
+                return 'object';
+            case PropertyTags::class:
+                return 'tags';
+            case PropertyTime::class:
+                return 'time';
+            case PropertyKeyfield::class:
+            case PropertyText::class:
+            case PropertyVarchar::class:
+                return 'string';
+        }
+    }
+    
+    protected function checkNativeValue($key)
+    {
+        if (!is_null($this->value) && !$key->isValid($this->value)) {
+            throw new WrongTypeException("The given search value is not compatible with the type of field '".$key->getName()."'");
+        }
+    }
+    
+    protected function checkArrayValues($key)
+    {
+        if (!is_array($this->value)) {
+            $this->value = [$this->value];
+        }
+        foreach ($this->value as $entry) {
+            if (!is_null($entry) && !$key->isValid($entry)) {
+                throw new WrongTypeException("The given search value is not compatible with the type of field '".$key->getName()."'");
+            }            
+        }
+    }
+    
+    protected function checkValidValue($key, $relation)
+    {
+        if ($relation['value'] == 'native') {
+            $this->checkNativeValue($key);
+        } else if ($relation['value'] == 'array') {
+            $this->checkArrayValues($key);
+        }
+    }
+
+    protected function invertConnection()
+    {
+        switch ($this->connection) {
+            case 'where':
+                $this->connection = 'whereNot';
+                break;
+            case 'whereNot':
+                $this->connection = 'where';
+                break;
+            case 'orWhere':
+                $this->connection = 'orWhereNot';
+                break;
+            case 'orWhereNot':
+                $this->connection = 'orWhere';
+                break;
+        }
+    }
+    
+    protected function dispatchArray()
+    {
+        switch ($this->relation) {
+            case '=':
+                return ['handleWhereArrayEquals', $this->connection, $this->key, $this->value];
+                break;
+            case '<>':
+                $this->invertConnection();
+                return ['handleWhereArrayEquals', $this->connection, $this->key, $this->value];
+                break;
+            case 'all of':
+                return ['handleWhereAllOf', $this->connection, $this->key, $this->value];
+            case 'any of':
+                return ['handleWhereAnyOf', $this->connection, $this->key, $this->value];
+            case 'none of':
+                return ['handleWhereNoneOf', $this->connection, $this->key, $this->value];
+            case 'all keys of':
+                return ['handleWhereAllKeysOf', $this->connection, $this->key, $this->value];
+            case 'any key of':
+                return ['handleWhereAnyKeyOf', $this->connection, $this->key, $this->value];
+            case 'none key of':
+                return ['handleWhereNoneKeyOf', $this->connection, $this->key, $this->value];
+        }
+    }
+    
+    protected function dispatchScalar()
+    {
+        switch ($this->relation) {
+            case 'in':
+                return ['handleWhereIn', $this->connection, $this->key, $this->value];
+            case 'null':
+                return ['handleWhereNull', $this->connection, $this->key];
+            case 'begins with':
+                return ['handleWhereLike', $this->connection, $this->key, $this->value.'%'];
+            case 'ends with':
+                return ['handleWhereLike', $this->connection, $this->key, '%'.$this->value];
+            case 'contains':
+                return ['handleWhereLike', $this->connection, $this->key, '%'.$this->value.'%'];
+            default:
+                return ['handleWhereSimple', $this->connection, $this->key, $this->relation, $this->value];
+        }
+    }
+    
+    protected function dispatchType($type)
+    {
+        switch ($type) {
+            case 'array':
+            case 'map':
+                return $this->dispatchArray();
+            case 'primitive':
+            case 'string':
+            case 'scalar':
+            case 'date':
+            case 'datetime':
+            case 'time':
+                return $this->dispatchScalar();
+            case 'collection':
+            case 'external':
+            case 'information':
+            case 'object':
+            case 'tags':
+        }
+            
+    }
+    
+    public function parseWhere(): array
+    {
+        if ($result = $this->checkUnaryRelation()) {
+            return $result;
+        }
+        if ($result = $this->checkSpecialKey()) {
+            return $result;
+        }
+        if (!($key = $this->checkKey())) {
+            if ($result = $this->checkAttribute()) {
+                return $result;
+            }
+            throw new UnknownFieldException();
+        }
+        $type = $this->getPropertyType($key);
+        $this->tryToTranslateRelation();
+        if (!isset($this->relations[$this->relation])) {
+            if (is_null($this->value)) {
+                $this->value    = $this->relation;
+                $this->relation = '=';
+            } else {
+                throw new NotAllowedRelationException("The relation '".$this->relation."' is not implemented.");
+            }
+        }
+        $relation = $this->relations[$this->relation];
+        if (!in_array($type, $relation['type'])) {
+            throw new NotAllowedRelationException("The relation '".$this->relation."' is not allowed for the type '$type'");
+        }
+        $this->checkValidValue($key, $relation);
+        return $this->dispatchType($type);
+    }
+    
+}
---
+++ tests/Unit/Storage/WhereParserTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Sunhill\ORM\Tests\Unit\Storage;
+
+use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\Tests\Testobjects\Dummy;
+use Sunhill\ORM\Storage\Mysql\WhereParser;
+use Sunhill\ORM\Query\NotAllowedRelationException;
+use Sunhill\ORM\Tests\Testobjects\TestParent;
+use Sunhill\ORM\Query\UnknownFieldException;
+use Sunhill\ORM\Query\TooManyWhereParametersException;
+use Sunhill\ORM\Facades\Classes;
+use Sunhill\ORM\Tests\DatabaseTestCase;
+use Sunhill\ORM\Facades\Attributes;
+use Sunhill\ORM\Query\WrongTypeException;
+
+class WhereParserTest extends TestCase
+{
+    
+    /**
+     * @dataProvider InputProvider
+     * @param unknown $connection
+     * @param unknown $key
+     * @param unknown $relation
+     * @param unknown $value
+     * @param unknown $expect
+     */
+    public function testInput($class, $connection, $key, $relation, $value, $expect)
+    {
+        $attribute = new \StdClass();
+        $attribute->name = 'int_attribute';
+        $attribute->type = 'integer';
+        $avaiable_attributes = collect([$attribute]);
+        Attributes::shouldReceive('getAvaiableAttributesForClass')->with(Dummy::class)->andReturn($avaiable_attributes);
+        
+        if (isset($expect[2]) && ($expect[2] == '*attribute*')) {
+            $expect[2] = $attribute;
+        }
+        $test = new WhereParser();
+        $test->setClass($class)->setConnection($connection)->setKey($key)->setRelation($relation)->setValue($value);
+        if (is_string($expect)) {
+            $this->expectException($expect);
+        }
+        $this->assertEquals($expect, $test->parseWhere());
+    }
+    
+    public static function InputProvider()
+    {
+        return [
+            [Dummy::class, 'where', 'dummyint', '=', 2,['handleWhereSimple','where', 'dummyint','=',2]],             // Trivial test
+            [Dummy::class, 'whereNot', 'dummyint', '=', 2,['handleWhereSimple','whereNot', 'dummyint','=',2]],       // Trivial test
+            [Dummy::class, 'orWhere', 'dummyint', '=', 2,['handleWhereSimple','orWhere', 'dummyint','=',2]],         // Trivial test
+            [Dummy::class, 'orWhereNot', 'dummyint', '=', 2,['handleWhereSimple','orWhereNot', 'dummyint','=',2]],   // Trivial test
+
+            [Dummy::class, 'where', 'dummyint', '=', null,['handleWhereSimple', 'where', 'dummyint', '=', null]],
+            
+            [Dummy::class, 'where', 'dummyint', 2, null,['handleWhereSimple','where', 'dummyint','=',2]],      // Test if default relation is =
+            [Dummy::class, 'where', 'dummyint', '<', 2, ['handleWhereSimple','where','dummyint','<',2]],            // Test standard relations for scalars             
+            [Dummy::class, 'where', 'dummyint', '<=', 2, ['handleWhereSimple','where','dummyint','<=',2]],          // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', '>=', 2, ['handleWhereSimple','where','dummyint','>=',2]],          // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', '>', 2, ['handleWhereSimple','where','dummyint','>',2]],            // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', '==', 2, ['handleWhereSimple','where','dummyint','=',2]],           // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', '<>', 2, ['handleWhereSimple','where','dummyint','<>',2]],          // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', '!=', 2, ['handleWhereSimple','where','dummyint','<>',2]],          // Test standard relations for scalars
+            
+            [Dummy::class, 'where', 'dummyint', 'in', [2,3], ['handleWhereIn','where','dummyint',[2,3]]],          // Test standard relations for scalars
+            [Dummy::class, 'where', 'dummyint', 'in', 2, ['handleWhereIn','where','dummyint',[2]]],          // Test standard relations for scalars
+            
+            [Dummy::class, 'where', 'dummyint', 'null', null, ['handleWhereNull', 'where', 'dummyint']],
+            
+            // Test unary conditions
+            [Dummy::class, 'where', 'has associations', null, null, ['handleWhereHasAssociations', 'where']],
+            [Dummy::class, 'where', 'is associated', null, null, ['handleWhereIsAssociated', 'where']],
+            [Dummy::class, 'where', 'has tags', null, null, ['handleWhereHasTags', 'where']],
+            [Dummy::class, 'where', 'has attributes', null, null, ['handleWhereHasAttributes', 'where']],
+
+            // String handling
+            [TestParent::class, 'where', 'parentchar', 'contains', 'A', ['handleWhereLike', 'where', 'parentchar', '%A%']],
+            [TestParent::class, 'where', 'parentchar', 'begins with', 'A', ['handleWhereLike', 'where', 'parentchar', 'A%']],
+            [TestParent::class, 'where', 'parentchar', 'ends with', 'A', ['handleWhereLike', 'where', 'parentchar', '%A']],
+            
+            // Array handling
+            [TestParent::class, 'where', 'parentsarray', 'all of', ['A'], ['handleWhereAllOf', 'where', 'parentsarray', ['A']]],
+            [TestParent::class, 'where', 'parentsarray', 'any of', ['A'], ['handleWhereAnyOf', 'where', 'parentsarray', ['A']]],
+            [TestParent::class, 'where', 'parentsarray', 'has', ['A'], ['handleWhereAnyOf', 'where', 'parentsarray', ['A']]],
+            [TestParent::class, 'where', 'parentsarray', 'none of', ['A'], ['handleWhereNoneOf', 'where', 'parentsarray', ['A']]],
+            [TestParent::class, 'where', 'parentsarray', 'all of', 'A', ['handleWhereAllOf', 'where', 'parentsarray', ['A']]],
+            [TestParent::class, 'where', 'parentsarray', '=', ['A','B'], ['handleWhereArrayEquals', 'where', 'parentsarray', ['A','B']]],
+            [TestParent::class, 'where', 'parentsarray', '<>', ['A','B'], ['handleWhereArrayEquals', 'whereNot', 'parentsarray', ['A','B']]],
+            
+            [TestParent::class, 'where', 'parentmap', 'all keys of', ['A'], ['handleWhereAllKeysOf', 'where', 'parentmap', ['A']]],
+            [TestParent::class, 'where', 'parentmap', 'any key of', ['A'], ['handleWhereAnyKeyOf', 'where', 'parentmap', ['A']]],
+            [TestParent::class, 'where', 'parentmap', 'none key of', ['A'], ['handleWhereNoneKeyOf', 'where', 'parentmap', ['A']]],
+            
+            // Attribute handling
+            [Dummy::class, 'where', 'int_attribute', '=', 3, ['handleWhereAttributeSimple','where','*attribute*','=',3]],
+            [Dummy::class, 'where', 'int_attribute', 3, null, ['handleWhereAttributeSimple','where','*attribute*','=',3]],
+            
+            // Test exceptions
+            [Dummy::class, 'where', 'dummyint', '?=', 2,NotAllowedRelationException::class],             // Unknown relation
+            [Dummy::class, 'where', 'dummyint', 'contains', 2,NotAllowedRelationException::class],       // Relation not allowed with this field
+            [Dummy::class, 'where', 'dummyint', 'all of', [2],NotAllowedRelationException::class],       // Relation not allowed with this field
+            [TestParent::class, 'where', 'parentsarray', '<', 'A', NotAllowedRelationException::class],
+            [Dummy::class, 'where', 'nonexisting', '=', 2,UnknownFieldException::class],                 // Field not known
+            [Dummy::class, 'where', 'has associations', 'A', null, TooManyWhereParametersException::class],
+            [Dummy::class, 'where', 'int_attribute', 'all of', 3, NotAllowedRelationException::class],
+            [Dummy::class, 'where', 'int_attribute', 'in', 3, WrongTypeException::class],
+        ];
+    }
+}
