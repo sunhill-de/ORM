413014e5decdab64e56fcc23551901bc9efa7818
Klaus Dimde <klaus@dimde.info>
2023-08-21 12:38:00 +0200

    Fixes objectmigrate

--- src/Storage/Mysql/Objects/MysqlObjectMigrate.php
+++ src/Storage/Mysql/Objects/MysqlObjectMigrate.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 use Sunhill\ORM\Properties\Property;
 use Sunhill\ORM\Facades\Classes;
 use Sunhill\ORM\Traits\PropertyUtils;
+use Sunhill\ORM\Storage\Mysql\Collections\MysqlCollectionMigrateFresh;
+use Sunhill\ORM\Storage\Mysql\MysqlAction;
+use Sunhill\ORM\Storage\Mysql\Collections\MysqlCollectionMigrateAlter;
 
-class MysqlObjectMigrate
+class MysqlObjectMigrate extends MysqlAction
 {
     
-    public function __construct(public $storage) {}
-
-    public function doMigrate()
+    public function run()
     {
-        if (Schema::hasTable($this->storage->getCaller()::getInfo('table'))) {
-            $migrator = new MysqlObjectMigrateAlter($this->storage);
+        $table = ($this->collection)::getInfo('table');
+        if (Schema::hasTable(($this->collection)::getInfo('table'))) {
+            $migrator = new MysqlCollectionMigrateAlter($this->collection);
         } else {
-            $migrator = new MysqlObjectMigrateFresh($this->storage);
+            $migrator = new MysqlCollectionMigrateFresh($this->collection);
         }
-        $migrator->doMigrate();
+        $migrator->setCollection($this->collection);
+        $migrator->run();
     }
         
 }
