f97af455f57c138956485cc9618468b0567c8206
Klaus Dimde <klaus@dimde.info>
2023-08-22 13:48:48 +0200

    alternative delete tag

--- tests/Feature/Objects/TagTest.php
+++ tests/Feature/Objects/TagTest.php
@@ -20,6 +20,7 @@ class TagTest extends DatabaseTestCase
 	    $tag = Tags::searchTag('TagA');
 	    $object->tags->stick('TagA');
 	    $object->commit();
+	//    Tags::query()->where('id',1)->delete();
 	    $tag = Tags::deleteTag(1);
 	    Objects::flushCache();
 	    $object = Objects::load($object->getID());
