9e6c9177d9ae68f19ba1dd03de794073e3a7fed6
Klaus Dimde <klaus@dimde.info>
2023-08-25 13:56:57 +0200

    Cleaned up code

--- src/Objects/ORMObject.php
+++ src/Objects/ORMObject.php
@@ -3,7 +3,7 @@
  * @file ORMObject.php
  * Provides the core object of the orm system named ORMObject
  * Lang en (complete)
- * Reviewstatus: 2023-05-09
+ * Reviewstatus: 2023-08-25
  * Localization: unknown
  * Documentation: unknown
  * Tests: unknown
@@ -14,23 +14,10 @@ namespace Sunhill\ORM\Objects;
 
 use Sunhill\ORM\Facades\Attributes;
 use Sunhill\ORM\Facades\ObjectData;
-use Sunhill\ORM\Facades\Objects;
 use Sunhill\ORM\Facades\Classes;
-use Sunhill\ORM\Facades\Tags;
-use Sunhill\ORM\Storage\StorageBase;
-use Sunhill\ORM\Search\QueryBuilder;
-use Sunhill\ORM\Properties\PropertyFloat;
 use Sunhill\ORM\Properties\PropertyInteger;
-use Sunhill\ORM\Properties\PropertyText;
-use Sunhill\ORM\Properties\PropertyTime;
 use Sunhill\ORM\Properties\PropertyVarchar;
-use Sunhill\ORM\Properties\PropertyBoolean;
-use Sunhill\ORM\Properties\PropertyDate;
 use Sunhill\ORM\Properties\PropertyDatetime;
-use Sunhill\ORM\Objects\StorageInteraction\StorageInteractionBase;
-use Sunhill\ORM\Objects\StorageInteraction\ObjectLoader;
-use Sunhill\ORM\Objects\StorageInteraction\ObjectStorer;
-use Sunhill\ORM\Objects\StorageInteraction\ObjectUpdater;
 
 /**
  * As the central class of the ORM system ORMObject provides the basic function for
@@ -74,87 +61,7 @@ class ORMObject extends PropertiesCollection
 	    return 'int';
 	}
 
-	protected function copyAttributes(StorageBase $storage)
-	{
-	    $result = [];
-	    foreach ($this->dynamic_properties as $key => $property) {
-	        $entry = new \StdClass();
-	        $entry->name = $key;
-	        $entry->attribute_id = $property->getAttributeID();
-	        $entry->value = $property->getValue();
-	        switch ($property::class) {
-	            case  PropertyInteger::class:
-	              $entry->type = 'integer'; break;
-	            case  PropertyVarchar::class:
-	                $entry->type = 'varchar'; break;
-	            case  PropertyFloat::class:
-	                $entry->type = 'float'; break;
-	            case  PropertyBoolean::class:
-	                $entry->type = 'boolean'; break;
-	            case  PropertyText::class:
-	                $entry->type = 'text'; break;
-	            case  PropertyDate::class:
-	                $entry->type = 'date'; break;
-	            case  PropertyDatetime::class:
-	                $entry->type = 'datetime'; break;
-	            case  PropertyTime::class:
-	                $entry->type = 'time'; break;
-	        }
-	        $result[] = $entry;
-	    }
-	    $storage->setEntity('attributes', $result);
-	}
-	
-	protected function preCreation(StorageBase $storage)
-	{
-	    $this->copyAttributes($storage);
-	}
-	
-	/**
-	 * Is called after the creation of an object. In this case copy the created values
-	 * uuid, created_at and updated_at back to the storage
-	 * @param StorageBase $storage
-	 */
-	protected function postCreation(StorageBase $storage)
-	{
-	    $this->uuid = $storage->getEntity('uuid');
-	    $this->created_at = $storage->getEntity('created_at');
-	    $this->updated_at = $storage->getEntity('updated_at');
-	}
-
-	protected function preUpdate(StorageBase $storage)
-	{
-	    
-	}
-	
-	protected function postUpdate(StorageBase $storage)
-	{
-	    
-	}
-		
 // ================================ Loading ========================================	
-	/**
-	 * Checks, if the object with ID $id is in the cache. If yes, return it, othwerwise return false
-	 * @param integer $id The id to search for
-	 * @return bool|ORMObject false if not in cache otherwise the cache entry
-	 */ 	 
-	protected function checkCache(int $id)
-    {
-	    if (Objects::isCached($id)) {
-	        return Objects::load($id);
-	    }
-	    return false;
-	}
-	
-	/**
-	 * Puts itself in the objects cache
-	 * @param Int $id
-	 */
-	protected function insertCache(int $id) 
-    {
-	    Objects::insertCache($id,$this);	    
-	}
-	
 	protected function prepareStore()
 	{
 	    $this->_uuid  = ObjectData::getUUID();
@@ -166,30 +73,6 @@ class ORMObject extends PropertiesCollection
 	{
 	    $this->_updated_at = ObjectData::getDBTime();
 	}
-	
-	
-// ========================= Insert =============================	
-	/**
-     * This method is called by the public method insert and 
-     * inserts an object into the storage
-	 * First for every property inserting is called, afterwards insert and finally inserted.
-	 */
-	protected function doInsert(StorageBase $storage, string $name)
-    {
-	       $storage = static::getStorage();
-           $this->walkProperties('insert',$storage);
-           $this->setID($storage->insertObject());
-           $this->insertCache($this->getID());
-	}
-
-// ========================== Update ===================================	
-	protected function doUpdate(StorageBase $storage, string $name) 
-    {
-	    $storage = static::getStorage();
-	    $storage->setEntity('id',$this->getID());
-	    $this->walkProperties('update',$storage);
-	    $storage->updateObject($this->getID());
-	}
 		
 	/**
 	 * Creates a new empty storage
@@ -198,97 +81,8 @@ class ORMObject extends PropertiesCollection
     {
 		
 	}
-	
-	// ================================= Delete =============================================
-	protected function doDelete() 
-    {
-	    $storage = static::getStorage();
-	    $this->walkProperties('delete',$storage);
-	    $storage->deleteObject($this->getID());
-	    $this->clearCacheEntry();
-	}
-	
-    /**
-     * Removes the entry from the cache
-     */
-	protected function clearCacheEntry() 
-    {
-	    Objects::clearCache($this->getID());
-	}
-	
-	// ********************* Property handling *************************************	
-	
-	/**
-	 * Recaculated all or one specific calculated fields. 
-	 * If $property is set, only this one is recalculated
-	 * @param unknown $property
-	 */
-	public function recalculate($property = null)
-    {
-	    if (!is_null($property)) {
-	        $property_obj = $this->getProperty($property);
-	        $property_obj->recalculate();
-	    } else {
-	        $properties = $this->getPropertiesWithFeature('calculated');
-	        foreach ($properties as $property) {
-	            $property->recalculate();
-	        }	        
-	    }
-	}
-	
-// =============================== Copying ====================================	
-	/**
-	 * This routine copies the properties to $newobject
-	 * @param ORMObject $newobject
-	 */
-	public function copyTo(ORMObject $newobject) 
-    {
-	    $newobject->setID($this->getID());
-	    foreach ($this->properties as $property) {
-	        $name = $property->getName();
-	        switch ($property::getType()) {
-	            case 'arrayOfObjects':
-	            case 'arrayOfStrings':
-	            case 'external_references':
-	            case 'tags':
-	                for ($i=0;$i<count($this->$name);$i++) {
-	                    $newobject->$name[] = $this->$name[$i];
-	                }
-	                break;
-	            case 'calculated':
-	                break;
-	            default:
-	                $newobject->$name = $this->$name;
-	        }
-	    }
-	}
-	
-	/**
-	 * This routine copies the properties of the $source to this object
-	 * @param ORMObject $source
-	 */
-	public function copyFrom(ORMObject $source) 
-    {
-	    $this->setID($source->getID());
-	    foreach ($this->properties as $property) {
-	        $name = $property->getName();
-	        switch ($property::getType()) {
-	            case 'arrayOfObjects':
-	            case 'arrayOfStrings':
-	            case 'external_references':
-	            case 'tags':
-	                for ($i=0;$i<count($source->$name);$i++) {
-	                    $this->$name[] = $source->$name[$i];
-	                }
-	                break;
-	            case 'calculated':
-	                break;
-	            default:
-	                $this->$name = $source->$name;
-	        }
-	    }
-	}
-	
+		
+	// ********************* Property handling *************************************		
     protected function searchAttribute($name)
     {
         $attributes = Attributes::getAvaiableAttributesForClass(static::class);
@@ -360,19 +154,4 @@ class ORMObject extends PropertiesCollection
 	    static::addInfo('description', 'A base class that defines storable properties.');
 	}
 
-	protected function getLoaderInteraction(): StorageInteractionBase
-	{
-	    return new ObjectLoader();
-	}
-	
-	protected function getStorerInteraction(): StorageInteractionBase
-	{
-	    return new ObjectStorer();
-	}
-	
-	protected function getUpdaterInteraction(): StorageInteractionBase
-	{
-	    return new ObjectUpdater();
-	}
-	
 }
