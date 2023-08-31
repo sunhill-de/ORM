f9c2f1660a0bc903eebb2a4c5fbad4930b38b0ad
Klaus Dimde <klaus@dimde.info>
2023-08-31 12:48:12 +0200

    Implemented marketeer test

--- tests/Unit/InfoMarket/MarketeerTest.php
+++ tests/Unit/InfoMarket/MarketeerTest.php
@@ -3,8 +3,37 @@
 namespace Sunhill\ORM\Tests\Unit\InfoMarket;
 
 use Sunhill\ORM\Tests\TestCase;
+use Sunhill\ORM\InfoMarket\Marketeer;
+
+class ParentMarketeer extends Marketeer
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->addEntry('child', DummyMarketeer::class);
+    }
+    
+}
 
 class MarketeerTest extends TestCase
 {
     
+    public function testGetItem()
+    {
+        $test = new DummyMarketeer();
+        
+        $item = $test->requestItem(['item1']);
+        $this->assertTrue(is_a($item, Item1::class));
+        $this->assertEquals('This is item 1', $item->getValue());
+    }
+    
+    public function testNestedGetItem()
+    {
+        $test = new ParentMarketeer();
+        
+        $item = $test->requestItem(['child','item1']);
+        $this->assertTrue(is_a($item, Item1::class));
+        $this->assertEquals('This is item 1', $item->getValue());
+    }
 }
