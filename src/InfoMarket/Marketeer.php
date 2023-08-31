d52566c81385530da76627777c91b9b51c2c50b4
Klaus Dimde <klaus@dimde.info>
2023-08-31 12:12:25 +0200

    Implemented addItem()

--- src/InfoMarket/Marketeer.php
+++ src/InfoMarket/Marketeer.php
@@ -8,9 +8,16 @@ use Sunhill\ORM\Properties\Property;
 class Marketeer extends NonAtomarProperty
 {
     
+    protected $entries = [];
+    
+    protected function addEntry(string $name, $entry)
+    {
+         $this->entries[$name] = $entry;
+    }
+    
     public function hasProperty(string $name): bool
     {
-        
+        return array_has_key($name, $this->entries);
     }
     
     public function getProperty(string $name): Property
