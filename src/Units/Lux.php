ff8e5c404097cb1bbacd8cf402e68f8e4f355f6c
Klaus Dimde <klaus@dimde.info>
2023-09-04 13:16:03 +0200

    Added some units

---
+++ src/Units/Lux.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file Lux.php
+ * A unit class that indicates a illuminance measured in lux
+ * Lang en
+ * Reviewstatus: 2023-05-03
+ * Localization: complete
+ * Documentation: complete
+ * Tests: Unit/Units/UnitTest.php
+ * Coverage: unknown
+ */
+
+namespace Sunhill\ORM\Units;
+
+class Lux extends Unit
+{
+    
+    protected static $name = 'Lux';
+    
+    protected static $unit = 'lx';
+    
+}
---
+++ src/Units/Torr.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file Torr.php
+ * A unit class that indicates a pressure measured in mmHg
+ * Lang en
+ * Reviewstatus: 2023-05-03
+ * Localization: complete
+ * Documentation: complete
+ * Tests: Unit/Units/UnitTest.php
+ * Coverage: unknown
+ */
+
+namespace Sunhill\ORM\Units;
+
+class Torr extends Unit
+{
+    
+    protected static $name = 'Torr';
+    
+    protected static $unit = 'mmHg';
+    
+}
