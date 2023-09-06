58b8f6fcc75a5ad452c69eca6e2d4f205bc542a2
Klaus Dimde <klaus@dimde.info>
2023-09-06 14:51:00 +0200

    Implemented registerCollection(), searchCollection() and getCollections()

--- src/Managers/CollectionManager.php
+++ src/Managers/CollectionManager.php
@@ -21,14 +21,23 @@ use Sunhill\ORM\Managers\Exceptions\IsNotACollectionException;
 class CollectionManager 
 {
     
-    public function loadCollection(string $class, int $id)
+    protected $collections = [];
+    
+    protected function checkCollection(string $class)
     {
+        $class = $this->searchCollection($class);
         if (!class_exists($class)) {
             throw new CollectionClassDoesntExistException("The given class '$class' doesn't exists.");
         }
         if (!is_a($class, Collection::class, true)) {
             throw new IsNotACollectionException("The given class '$class' is not a collection.");
         }
+    }
+    
+    public function loadCollection(string $class, int $id)
+    {
+        $this->checkCollection($class);
+        
         $object = new $class();
         $object->load($id);
         
@@ -38,5 +47,38 @@ class CollectionManager
     public function deleteCollection(string $class, int $id)
     {
         
+    }
+    
+    /**
+     * To find collections via their name they should be registered
+     * @param string $collection
+     */
+    public function registerCollection(string $collection)
+    {
+        $this->checkCollection($collection);
+        
+        $this->collections[$collection::getInfo('name')] = $collection;    
+    }
+    
+    /**
+     * Searches for a collection either via its name or via its namespace
+     * @param string $name
+     * @throws IsNotACollectionException
+     * @return string The namespace of the collection
+     */
+    public function searchCollection(string $name)
+    {
+        if (isset($this->collections[$name])) {
+            return $this->collections[$name];
+        }
+        if (is_a($name, Collection::class, true)) {
+            return $name;
+        }
+        throw new IsNotACollectionException("The given class '$name' is not the name of a collection.");
+    }
+    
+    public function getRegisteredCollections()
+    {
+        return $this->collections;
     }
 }
