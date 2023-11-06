3d7fd8a4e12977839dcf18ef1daea34750a1be83
Klaus Dimde <klaus@dimde.info>
2023-11-06 22:29:33 +0100

    Laid out the common methods

--- src/Managers/ClassManager.php
+++ src/Managers/ClassManager.php
@@ -27,6 +27,7 @@ use Sunhill\ORM\Query\BasicQuery;
 use Sunhill\ORM\Managers\Exceptions\ClassNotAccessibleException;
 use Sunhill\ORM\Managers\Exceptions\ClassNotORMException;
 use Sunhill\ORM\Managers\Exceptions\ClassNameForbiddenException;
+use Sunhill\ORM\Managers\ClassQuery\ClassQuery;
 
  /**
   * Wrapper class for handling of objectclasses. It provides some public static methods to get informations about
@@ -40,32 +41,12 @@ use Sunhill\ORM\Managers\Exceptions\ClassNameForbiddenException;
   * @author lokal
   * Test: Unit/Managers/ManagerClassesTest.php
   */
-class ClassManager extends RegistableManagerBase
+class ClassManager extends PropertiesCollectionManager
 {
  
-    const FORBIDDEN_NAMES = ['object','class','integer','string','float','boolean','tag'];
-    const FORBIDDEN_BEGINNINGS = ['attr_'];
             
 // ********************************** Register class ******************************************
         
-    /**
-     * Get the class informations and adds them to $result
-     * 
-     * @param $result The array to store the information to
-     * @param $class The full namespace of the class (not the class name!)
-     * 
-     * Test: testGetClassInformationEntries
-     */
-    private function getClassInformationEntries(\StdClass $result,string $class): void 
-    {
-        foreach ($class::getAllInfos() as $key => $value) {
-            if ($value->translatable) {
-                $result->$key = __($value->value);
-            } else {
-                $result->$key = $value->value;
-            }
-        }
-    }
     
     /**
      * Returns the parent entry of this class
@@ -75,7 +56,7 @@ class ClassManager extends RegistableManagerBase
      * 
      * Test: testGetClassParentEntry
      */
-    private function getClassParentEntry(\StdClass $result,string $class): void 
+    protected function buildAdditionalInformation(\StdClass $result,string $class): void 
     {
         $parent = get_parent_class($class);
         if ($class !== ORMObject::class) { 
@@ -84,82 +65,7 @@ class ClassManager extends RegistableManagerBase
             $result->parent = '';            
         }
     }
-    
-    /**
-     * Return all properties of the given class
-     * 
-     * @param string $class The full namespace of the class (not the class name!)
-     * @return array The properties of the given class
-     * 
-     * Test: testGetClassProperties
-     */
-    private function getClassProperties(string $class): array 
-    {
-        $properties = $class::getAllPropertyDefinitions();
-        $result = [];
-        foreach ($properties as $name => $descriptor) {
-            if ($name !== 'tags') {
-                $result[$name] = $descriptor;
-            }
-        }
-        return $result;
-    }
-    
-    /**
-     * Inserts the class properties in the result array
-     * 
-     * @param array $result The array to store the information to
-     * @param string $class The full namespace of the class (not the class name!)
-     * 
-     * Test: testGetClassPropertyEntries
-     */
-    private function getClassPropertyEntries(\StdClass &$result,string $class): void 
-    {
-        $result->properties = [];
-        $properties = $this->getClassProperties($class);
-        foreach ($properties as $property) {
-            $result->properties[$property->getName()] = [];
-            $features = $property->getAttributes();
-            foreach ($features as $feat_key => $feat_value) {
-                $result->properties[$property->getName()][$feat_key] = $feat_value;
-            }            
-        }    
-   }
-    
-    /**
-     * Collects all data about this class to store it in the classes array
-     * 
-     * @param $classname string The name of the class to collect values from
-     * @return array associative array with informations about this class
-     * 
-     * test: testBuildClassInformation
-     */
-    private function buildClassInformation(string $classname): \StdClass 
-    {
-        $result = new \StdClass();
-        $result->class = $classname;
-        
-        $this->getClassInformationEntries($result,$classname);
-        $this->getClassParentEntry($result,$classname);
-        $this->getClassPropertyEntries($result,$classname);
-        
-        return $result;
-     }
-    
-     /**
-      * Checks if the given classpath even exists
-      * @param string $classpath
-      * @throws ClassNotAccessibleException
-      * @return boolean
-      */
-     protected function checkClassExistance(string $classpath)
-     {
-         if (!class_exists($classpath)) {
-             throw new ClassNotAccessibleException("The class '$classpath' is not accessible.");
-             return false;
-         }
-     }
-     
+       
      /**
       * Checks if the given classpath is a descendant of ORMObject
       * @param string $classpath
@@ -172,43 +78,6 @@ class ClassManager extends RegistableManagerBase
          }
      }
 
-     /**
-      * Checks if the given classname is allowed
-      * @param string $classpath
-      * @return bool
-      */
-     protected function isClassNameForbidden(string $classname): bool
-     {
-        return in_array($classname, ClassManager::FORBIDDEN_NAMES);    
-     }
-     
-     /**
-      * Checks if the classname begins with a forbidden string
-      * @param string $classpath
-      * @return bool
-      */
-     protected function isClassBeginningForbidden(string $classname): bool
-     {
-         foreach (ClassManager::FORBIDDEN_BEGINNINGS as $beginning) {
-             if (substr($classname,0,strlen($beginning)) == $beginning) {
-                 return true;
-             }
-         }
-         return false;
-     }
-     
-     /**
-      * Checks if the classname is allowed
-      * @param string $classpath
-      * @throws ClassNameForbiddenException
-      */
-     protected function checkClassName(string $classpath)
-     {
-         if ($this->isClassNameForbidden(strtolower($classpath::getInfo('name'))) || $this->isClassBeginningForbidden(strtolower($classpath::getInfo('name')))) {
-             throw new ClassNameForbiddenException("The classname '".$classpath::getInfo('name')."' is no allowed.");
-         }
-     }
-     
     /**
      * Every single class that should be accessible via the class manager should be added through this method. 
      * It is possible to use an ORMObject without registering even store it but all references to other
@@ -233,37 +102,7 @@ class ClassManager extends RegistableManagerBase
         'object'=>$this->getItemInformation(ORMObject::class)
         ];
     }
-    
-    protected function checkValidity($item)
-    {
-        $this->checkClassExistance($item);
-        $this->checkClassType($item);
-        $this->checkClassName($item);        
-    }
-    
-    protected function getItemInformation($item)
-    {
-        return $this->buildClassInformation($item);
-    }
-    
-    protected function getItemKey($item): string
-    {
-        return $item::getInfo('name');
-    }
-    
-    /**
-     * Clears the class information array and insert the ORMObject by hand
-     * @todo: buildClassInformation builds an infinite loop. So this is done by hand
-     * Test: testFlushClasses
-     */
-    public function flushClasses(): void 
-    {
-        $this->registered_items = [
-            'object'=>
-            $this->buildClassInformation(ORMObject::class)
-        ];
-    }
-    
+        
 // *************************** General class informations ===============================    
     /**
      * Returns the number of registered classes
--- src/Managers/CollectionManager.php
+++ src/Managers/CollectionManager.php
@@ -18,27 +18,27 @@ use Sunhill\ORM\Managers\Exceptions\CollectionClassDoesntExistException;
 use Sunhill\ORM\Objects\Collection;
 use Sunhill\ORM\Managers\Exceptions\IsNotACollectionException;
 use Sunhill\ORM\Query\BasicQuery;
+use Sunhill\ORM\Managers\Exceptions\ClassNotORMException;
+use Sunhill\ORM\Managers\CollectionQuery\CollectionQuery;
 
-class CollectionManager 
+class CollectionManager extends PropertiesCollectionManager
 {
     
-    protected $collections = [];
-    
-    protected function checkCollection(string $class)
+    /**
+     * Checks if the given classpath is a descendant of ORMObject
+     * @param string $classpath
+     * @throws ClassNotORMException
+     */
+    protected function checkClassType(string $classpath)
     {
-        $class = $this->searchCollection($class);
-        if (!class_exists($class)) {
-            throw new CollectionClassDoesntExistException("The given class '$class' doesn't exists.");
+        if (!is_a($classpath, Collection::class,true)) {
+            throw new ClassNotORMException("The class '$classpath' is not a descendant of Collection");
         }
-        if (!is_a($class, Collection::class, true)) {
-            throw new IsNotACollectionException("The given class '$class' is not a collection.");
-        }
-        return $class;
     }
     
     public function loadCollection(string $class, int $id)
     {
-        $class = $this->checkCollection($class);
+        $class = $this->checkValidity($class);
                 
         $object = new $class();
         $object->load($id);
@@ -48,7 +48,7 @@ class CollectionManager
     
     public function collectionExists(string $class, int $id)
     {
-        $class = $this->checkCollection($class);
+        $class = $this->checkValidity($class);
         
         return $class::IDExists($id);
     }
@@ -59,112 +59,12 @@ class CollectionManager
     }
     
     /**
-     * Get the class informations and adds them to $result
-     *
-     * @param $result The array to store the information to
-     * @param $class The full namespace of the class (not the class name!)
-     *
-     * Test: testGetClassInformationEntries
-     */
-    private function getClassInformationEntries(\StdClass $result,string $class): void
-    {
-        foreach ($class::getAllInfos() as $key => $value) {
-            if ($value->translatable) {
-                $result->$key = __($value->value);
-            } else {
-                $result->$key = $value->value;
-            }
-        }
-    }
-    
-    /**
-     * Returns the parent entry of this class
-     *
-     * @param array $result The array to store the information to
-     * @param string $class The full namespace of the class (not the class name!)
-     *
-     * Test: testGetClassParentEntry
-     */
-    private function getClassParentEntry(\StdClass $result,string $class): void
-    {
-        $parent = get_parent_class($class);
-        if ($class !== ORMObject::class) {
-            $result->parent = $parent::getInfo('name');
-        } else {
-            $result->parent = '';
-        }
-    }
-    
-    /**
-     * Return all properties of the given class
-     *
-     * @param string $class The full namespace of the class (not the class name!)
-     * @return array The properties of the given class
-     *
-     * Test: testGetClassProperties
-     */
-    private function getClassProperties(string $class): array
-    {
-        $properties = $class::getAllPropertyDefinitions();
-        $result = [];
-        foreach ($properties as $name => $descriptor) {
-            if ($name !== 'tags') {
-                $result[$name] = $descriptor;
-            }
-        }
-        return $result;
-    }
-    
-    /**
-     * Inserts the class properties in the result array
-     *
-     * @param array $result The array to store the information to
-     * @param string $class The full namespace of the class (not the class name!)
-     *
-     * Test: testGetClassPropertyEntries
-     */
-    private function getClassPropertyEntries(\StdClass &$result,string $class): void
-    {
-        $result->properties = [];
-        $properties = $this->getClassProperties($class);
-        foreach ($properties as $property) {
-            $result->properties[$property->getName()] = [];
-            $features = $property->getAttributes();
-            foreach ($features as $feat_key => $feat_value) {
-                $result->properties[$property->getName()][$feat_key] = $feat_value;
-            }
-        }
-    }
-    /**
-     * Collects all data about this class to store it in the classes array
-     *
-     * @param $classname string The name of the class to collect values from
-     * @return array associative array with informations about this class
-     *
-     * test: testBuildClassInformation
-     */
-    private function buildClassInformation(string $classname): \StdClass
-    {
-        $result = new \StdClass();
-        $result->class = $classname;
-        
-        $this->getCollectionInformationEntries($result,$classname);
-        $this->getCollectionPropertyEntries($result,$classname);
-        
-        return $result;
-    }
-    
-    /**
      * To find collections via their name they should be registered
      * @param string $collection
      */
     public function registerCollection(string $collection)
     {
-        $this->checkCollection($collection);
-        
-        $information = $this->buildCollectionInformation($classname);
-        
-        $this->collections[$collection::getInfo('name')] = $information;    
+        $this->register($collection);
     }
     
     /**
@@ -175,8 +75,8 @@ class CollectionManager
      */
     public function searchCollection(string $name)
     {
-        if (isset($this->collections[$name])) {
-            return $this->collections[$name];
+        if (isset($this->registered_items[$name])) {
+            return $this->registered_items[$name];
         }
         if (is_a($name, Collection::class, true)) {
             return $name;
@@ -184,14 +84,14 @@ class CollectionManager
         throw new IsNotACollectionException("The given class '$name' is not the name of a collection.");
     }
     
-    public function getRegisteredCollections()
+    public function getAllCollections()
     {
-        return $this->collections;
+        return $this->registered_items;
     }
     
     public function migrateCollections()
     {
-        foreach ($this->collections as $name => $namespace) {
+        foreach ($this->registered_items as $name => $namespace) {
             $namespace::migrate();
         }
     }
