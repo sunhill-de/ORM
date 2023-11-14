e1993cc7c5715ea4e84f496663137a40b1dcb246
Klaus Dimde <klaus@dimde.info>
2023-11-14 15:43:12 +0100

    Fixed arrays for named entries too

--- src/InfoMarket/Items/ArrayInfoMarketItem.php
+++ src/InfoMarket/Items/ArrayInfoMarketItem.php
@@ -7,6 +7,42 @@ use Sunhill\ORM\Properties\AtomarProperty;
 abstract class ArrayInfoMarketItem extends AtomarProperty
 {
 
+    static protected $type = 'array';
+    
+    static protected $named_array = false;
+    
+    protected function initializeValue(): bool
+    {
+        return true;        
+    }
+    
+    protected function &doGetValue()
+    {
+        $result = ['count'=>'count'];
+        
+        if (static::$named_array) {
+            $result = array_merge($result, $this->getNamedEntries());
+        } else {
+            $result = array_merge($result, $this->getNumberedEntries());
+        }
+        return $result;        
+    }
+    
+    protected function getNamedEntries()
+    {
+        return [];
+    }
+    
+    protected function getNumberedEntries()
+    {
+        $result = [];
+        for ($i=0;$i<$this->getCountValue();$i++) {
+            $result[] = $i;
+        }
+        
+        return $result;
+    }
+    
     /**
      * Should return the element count of values of this array item
      * @return int
@@ -19,10 +55,14 @@ abstract class ArrayInfoMarketItem extends AtomarProperty
      */
     abstract protected function getIndexedElement(int $index);
     
+    protected function getNamedElement(string $name)
+    {
+    }
+    
     protected function getCount()
     {
         $result = $this->createResponseFromValue($this->getCountValue());
-        return $result->OK()->type('int')->unit('none')->semantic('count')->readable()->writeable(false)->update('asap');        
+        return $result->OK()->type('int')->unit('None')->semantic('Count')->readable()->writeable(false)->update('asap');        
     }
     
     protected function requestTerminalItem(string $name)
@@ -33,6 +73,7 @@ abstract class ArrayInfoMarketItem extends AtomarProperty
         if (is_numeric($name)) {
             return $this->getIndexedElement(intval($name));
         }
+        return $this->getNamedElement($name);
     }
     
 }
