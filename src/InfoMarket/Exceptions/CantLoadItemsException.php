a4fc3bb726716aa1d7f37f36e8b9cafdf7adfe95
Klaus Dimde <klaus@dimde.info>
2023-11-15 19:56:10 +0100

    Two additional exceptions for infomarket-items

---
+++ src/InfoMarket/Exceptions/CantLoadItemsException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Sunhill\ORM\InfoMarket\Exceptions;
+
+class CantLoadItemsException extends InfoMarketException {}
---
+++ src/InfoMarket/Exceptions/InvalidItemException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Sunhill\ORM\InfoMarket\Exceptions;
+
+class InvalidItemException extends InfoMarketException {}
