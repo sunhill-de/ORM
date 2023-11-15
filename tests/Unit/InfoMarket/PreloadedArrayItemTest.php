8245eda9464a2aa1d8af1a06fcc8fb8d60d35c72
Klaus Dimde <klaus@dimde.info>
2023-11-15 20:24:20 +0100

    Test for named arrays

--- tests/Unit/InfoMarket/PreloadedArrayItemTest.php
+++ tests/Unit/InfoMarket/PreloadedArrayItemTest.php
@@ -25,6 +25,20 @@ class DummyPreloadedArrayItem extends PreloadedArrayItem
     
 }
 
+class NamedDummyPreloadedArrayItem extends PreloadedArrayItem
+{
+    
+    protected function loadItems(): array
+    {
+        return [
+            'A'=>$this->createResponseFromValue('AAA')->OK()->type('string')->unit('None')->semantic('Name')->readable()->writeable(false)->update('asap'),
+            'B'=>$this->createResponseFromValue('BBB')->OK()->type('string')->unit('None')->semantic('Name')->readable()->writeable(false)->update('asap'),
+            'C'=>$this->createResponseFromValue('CCC')->OK()->type('string')->unit('None')->semantic('Name')->readable()->writeable(false)->update('asap')
+        ];
+    }
+    
+}
+
 class PreloadedArrayItemTest extends TestCase
 {
 
@@ -45,4 +59,29 @@ class PreloadedArrayItemTest extends TestCase
         $test = new DummyPreloadedArrayItem();
         $this->assertEquals('BBB', $test->requestItem(['1'])->getElement('value'));
     }
+    
+    public function testNamedGetOffering()
+    {
+        $test = new NamedDummyPreloadedArrayItem();
+        $this->assertEquals(['count','A','B','C'], $test->getValue());
+    }
+    
+    public function testNamedGetCount()
+    {
+        $test = new NamedDummyPreloadedArrayItem();
+        $this->assertEquals(3,$test->requestItem(['count'])->getElement('value'));
+    }
+    
+    public function testNamedGetElement()
+    {
+        $test = new NamedDummyPreloadedArrayItem();
+        $this->assertEquals('BBB', $test->requestItem(['B'])->getElement('value'));
+    }
+    
+    public function testNamedGetElementByIndex()
+    {
+        $test = new NamedDummyPreloadedArrayItem();
+        $this->assertEquals('BBB', $test->requestItem([1])->getElement('value'));
+    }
+        
 }
