27db1924eec241411a5d6ada49a8f3ee5f284a0c
Klaus Dimde <klaus@dimde.info>
2023-12-28 17:59:56 +0100

    Added sematic status

---
+++ src/Semantic/Status.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file Statuc.php
+ * A semantic class for describing the status of something
+ * Lang en
+ * Reviewstatus: 2023-12-28
+ * Localization: complete
+ * Documentation: complete
+ * Tests: Unit/Semantic/SemanticTest.php
+ * Coverage: unknown
+ */
+
+namespace Sunhill\ORM\Semantic;
+
+class Status extends Semantic
+{
+    protected static $name = 'Status';
+    
+}
+ 
