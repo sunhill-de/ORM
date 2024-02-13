d9bcbb5186cea213fa5c8234cdec215aa1942024
Klaus Dimde <klaus@dimde.info>
2024-02-13 05:19:51 +0100

    Moved test storage in own support file, because it can be reused

--- tests/Unit/Storage/AbstractIDStorageTest.php
+++ tests/Unit/Storage/AbstractIDStorageTest.php
@@ -4,62 +4,7 @@ namespace Sunhill\ORM\Tests\Unit\Managers;
 
 use Sunhill\ORM\Tests\TestCase;
 use Sunhill\ORM\Storage\AbstractIDStorage;
-
-class TestAbstractIDStorage extends AbstractIDStorage
-{
-    
-    public $data = [['test_str'=>'ABC','test_int'=>123],['test_str'=>'DEF','test_int'=>345]];
-    
-    public $next_id = 2;
-    
-    public function getReadCapability(string $name): ?string
-    {
-        return null; // No need to test
-    }
-    
-    public function getIsReadable(string $name): bool
-    {
-        return true;
-    }
-    
-    protected function doGetValue(string $name)
-    {
-        return $this->values[$name];
-    }
-    
-    public function getWriteCapability(string $name): ?string
-    {
-        return null;
-    }
-    
-    public function getWriteable(string $name): bool
-    {
-        return true;
-    }
-    
-    public function getModifyCapability(string $name): ?string
-    {
-        return null;
-    }
-    
-    protected function readFromID(int $id)
-    {
-        $this->values = $this->data[$id];
-    }
-    
-    protected function writeToID(): int
-    {
-        $this->data[$this->next_id] = $this->values;
-        return $this->next_id++;
-    }
-    
-    protected function updateToID(int $id)
-    {
-        $this->data[$id] = $this->values;        
-    }
-        
-}
-
+use Sunhill\ORM\Tests\TestSupport\TestAbstractIDStorage;
 
 class AbstractIDStorageTest extends TestCase
 {
