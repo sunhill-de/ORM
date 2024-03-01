2954e696f270b1d5690a9d1ed3194b8466492466
Klaus Dimde <klaus@dimde.info>
2024-03-01 11:54:04 +0100

    Implemented precision handling

--- src/Properties/Types/TypeFloat.php
+++ src/Properties/Types/TypeFloat.php
@@ -4,7 +4,7 @@
  * @file TypeFloat.php
  * Defines a type for floats
  * Lang en
- * Reviewstatus: 2024-02-05
+ * Reviewstatus: 2024-02-28
  * Localization: complete
  * Documentation: complete
  * Tests: 
@@ -16,6 +16,27 @@ namespace Sunhill\ORM\Properties\Types;
 class TypeFloat extends TypeNumeric
 {
    
+    /**
+     * Tells getHumanValue how many digits after the comma should be diaplayed
+     * 
+     * @var integer
+     */
+    protected $precision = 2;
+    
+    public function setPrecision(int $digits)
+    {
+       $this->precision = $digits; 
+    }
+    
+    /**
+     * Getter for precision 
+     * @return int
+     */
+    public function getPrecision(): int
+    {
+        return $this->precision;    
+    }
+    
     protected function isNumericType($input): bool
     {
         return is_numeric($input);
@@ -26,4 +47,8 @@ class TypeFloat extends TypeNumeric
         return 'float';
     }
     
+    protected function formatForHuman($input)
+    {
+        return round($input, $this->precision);
+    }
 }
